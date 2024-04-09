d09a406705458394d5f67477176322d9ef7cae99
Rojan Rajbhandari <rrojan20@gmail.com>
2024-04-09 13:14:13 +0545

    feat: add workspaceIds and remove hidden statuses

---
+++ prisma/migrations/20240409072855_remove_hidden_statuses/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `hiddenStatuses` on the `ViewSettings` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "ViewSettings" DROP COLUMN "hiddenStatuses";
