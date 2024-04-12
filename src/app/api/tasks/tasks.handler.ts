080a3159552d7a0460481fdcfc1ec61414f86f21
Rojan Rajbhandari <rrojan20@gmail.com>
2024-04-12 10:41:44 +0545

    feat: implement authorization in handler

--- src/app/api/tasks/tasks.handler.ts
+++ src/app/api/tasks/tasks.handler.ts
@@ -2,12 +2,17 @@ import { NextRequest, NextResponse } from 'next/server'
 import { TasksService } from './tasks.service'
 import { NextFn, Params } from '@/lib/plumber/types'
 import { AuthenticatedParams } from '../core/types/context'
+import { Routes } from '../core/types/api'
 
 export const getTasks = async (req: NextRequest, params?: Params, next?: NextFn) => {
-  console.log(params)
+  const { user } = params as AuthenticatedParams
   const tasksService = new TasksService()
+  if (!user.can('read', Routes.Tasks)) {
+    throw new APIError(401, 'You are not authorized to perform this action')
+  }
+
   const tasks = tasksService.getAllTasks({
-    workspaceId: (params as AuthenticatedParams)?.currentUser.workspaceId as string,
+    workspaceId: user.workspaceId,
   })
 
   return NextResponse.json({ tasks })
