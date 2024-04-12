2d43c561c3b477dd22a8daf58bab0edc1458669a
Rojan Rajbhandari <rrojan20@gmail.com>
2024-04-12 10:41:26 +0545

    feat: bind policies service to User model

--- src/app/api/core/models/User.model.ts
+++ src/app/api/core/models/User.model.ts
@@ -1,5 +1,7 @@
-import { UserRole } from '@/types/api'
+import { UserAction, UserRole } from '@api/core/types/user'
 import { Token } from '@/types/common'
+import { PoliciesService } from '@api/core/services/policies.service'
+import { Routes } from '@api/core/types/api'
 
 class User implements Token {
   token: string
@@ -8,6 +10,7 @@ class User implements Token {
   clientId?: string
   companyId?: string
   internalUserId?: string
+  can: (action: UserAction, route: Routes) => boolean
 
   constructor(token: string, tokenPayload: Token) {
     this.token = token
@@ -16,6 +19,9 @@ class User implements Token {
     this.clientId = tokenPayload.clientId
     this.companyId = tokenPayload.companyId
     this.workspaceId = tokenPayload.workspaceId
+
+    const policiesService = new PoliciesService(this)
+    this.can = policiesService.can.bind(policiesService)
   }
 }
 
