90f44cd1403186e84e9b609069beecc4a2482883
Rojan Rajbhandari <rrojan20@gmail.com>
2024-04-14 16:10:26 +0545

    fix: fix client task fetch logic

--- src/app/api/tasks/tasks.service.ts
+++ src/app/api/tasks/tasks.service.ts
@@ -2,28 +2,46 @@ import { CreateTaskRequest, UpdateTaskRequest } from '@/types/dto/tasks.dto'
 import { BaseService } from '@api/core/services/base.service'
 import { Resource } from '@api/core/types/api'
 import { UserRole } from '@api/core/types/user'
-import APIError from '@api/core/exceptions/api'
 import { PoliciesService } from '../core/services/policies.service'
+import { AssigneeType } from '@prisma/client'
+
+type FilterByAssigneeId = {
+  assigneeId: string
+  assigneeType: AssigneeType
+}
 
 export class TasksService extends BaseService {
+  private buildReadFilters() {
+    const user = this.user
+
+    let filters = { where: { workspaceId: user.workspaceId, OR: undefined as unknown as FilterByAssigneeId[] } }
+
+    if (user.clientId) {
+      filters = {
+        where: {
+          ...filters.where,
+          OR: [{ assigneeId: user.clientId as string, assigneeType: 'client' }],
+        },
+      }
+    }
+    if (user.clientId && user.companyId) {
+      filters = {
+        where: {
+          ...filters.where,
+          OR: [
+            { assigneeId: user.clientId as string, assigneeType: 'client' },
+            { assigneeId: user.companyId, assigneeType: 'company' },
+          ],
+        },
+      }
+    }
+
+    return filters
+  }
   async getAllTasks() {
     const user = this.user
     new PoliciesService(user).authorize('read', Resource.Tasks)
-
-    const filters = { where: { workspaceId: user.workspaceId } }
-    if (user.role === UserRole.Client) {
-      // @ts-expect-error fix in future
-      filters.where.assigneeId = user.clientId
-      // @ts-expect-error fix in future
-      filters.where.assigneeType = 'client'
-      if (user.companyId) {
-        // @ts-expect-error fix in future
-        filters.where.OR = [
-          { assigneeId: user.clientId, assigneeType: 'client' },
-          { assigneeId: user.companyId, assigneeType: 'company' },
-        ]
-      }
-    }
+    const filters = this.buildReadFilters()
 
     return await this.db.task.findMany({
       ...filters,
@@ -50,20 +68,7 @@ export class TasksService extends BaseService {
     const user = this.user
     new PoliciesService(user).authorize('read', Resource.Tasks)
 
-    const filters = { where: { workspaceId: user.workspaceId } }
-    if (user.role === UserRole.Client) {
-      // @ts-expect-error fix in future
-      filters.where.assigneeId = user.clientId
-      // @ts-expect-error fix in future
-      filters.where.assigneeType = 'client'
-      if (user.companyId) {
-        // @ts-expect-error fix in future
-        filters.where.OR = [
-          { assigneeId: user.clientId, assigneeType: 'client' },
-          { assigneeId: user.companyId, assigneeType: 'company' },
-        ]
-      }
-    }
+    const filters = this.buildReadFilters()
 
     return await this.db.task.findFirst({
       ...filters,
