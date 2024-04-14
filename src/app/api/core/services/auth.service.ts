185bb9692ef221319d4487ac44042375ad18d647
Rojan Rajbhandari <rrojan20@gmail.com>
2024-04-14 16:12:56 +0545

    fix: use http status

--- src/app/api/core/services/auth.service.ts
+++ src/app/api/core/services/auth.service.ts
@@ -4,6 +4,7 @@ import User from '@api/core/models/User.model'
 import { z } from 'zod'
 import { TokenSchema } from '@/types/common'
 import APIError from '@api/core/exceptions/api'
+import httpStatus from 'http-status'
 
 /**
  * Token parser and authentication service
@@ -13,14 +14,14 @@ class AuthService {
     const token = req.nextUrl.searchParams.get('token')
     const tokenParsed = z.string().safeParse(token)
     if (!tokenParsed.success) {
-      throw new APIError(401, 'Please provide a valid token')
+      throw new APIError(httpStatus.UNAUTHORIZED, 'Please provide a valid token')
     }
 
     const copilotClient = new CopilotAPI(tokenParsed.data)
     const payload = TokenSchema.safeParse(await copilotClient.getTokenPayload())
 
     if (!payload.success) {
-      throw new APIError(401, 'Failed to authenticate token')
+      throw new APIError(httpStatus.UNAUTHORIZED, 'Failed to authenticate token')
     }
 
     return new User(tokenParsed.data, payload.data)
