b557ae4537cfb5fda155772b0aac95035c9d34cc
aatbip <anantabipal2010@gmail.com>
2024-04-15 16:59:32 +0545

    refac: made keys more typesafe

--- src/utils/iconMatcher.tsx
+++ src/utils/iconMatcher.tsx
@@ -1,11 +1,13 @@
 import { DoneIcon, InprogressIcon, InreviewIcon, PdfIcon, PngIcon, TodoIcon } from '@/icons'
 import { ReactNode } from 'react'
 
+type StatusKey = 'Todo' | 'In Progress' | 'In Review' | 'Done'
+
 //This is mock data which will be replaced after API integration.
-export const statusIcons: { [key: string]: ReactNode } = {
+export const statusIcons: { [key in StatusKey]: ReactNode } = {
   Todo: <TodoIcon />,
   'In Progress': <InprogressIcon />,
-  'In review': <InreviewIcon />,
+  'In Review': <InreviewIcon />,
   Done: <DoneIcon />,
 }
 
