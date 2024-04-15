5a4e21675fe03107c6b5a584b4774eaf5979cfba
aatbip <anantabipal2010@gmail.com>
2024-04-15 17:46:11 +0545

    refac: removed DndWrapper from top layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
+import { DndWrapper } from '@/hoc/DndWrapper'
 import { TaskBoard } from './ui/TaskBoard'
 import { Header } from '@/components/layouts/Header'
 
 export default function Main() {
   return (
     <>
-      <Header />
-      <TaskBoard />
+      <DndWrapper>
+        <Header />
+        <TaskBoard />
+      </DndWrapper>
     </>
   )
 }
