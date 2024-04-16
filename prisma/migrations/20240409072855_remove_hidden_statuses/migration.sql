93fac861ac0cb16ba569a15f7911fa7cfb83609d
Rojan Rajbhandari <rrojan20@gmail.com>
2024-04-16 15:08:34 +0545

    feat: change schema to reflect new design

--- prisma/migrations/20240409072855_remove_hidden_statuses/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `hiddenStatuses` on the `ViewSettings` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "ViewSettings" DROP COLUMN "hiddenStatuses";
