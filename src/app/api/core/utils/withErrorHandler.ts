98822f533fbbf09e310b40989d025e9adfe0d0c3
Rojan Rajbhandari <rrojan20@gmail.com>
2024-04-16 16:15:09 +0545

    fix: catch zod error in withErrorHandler

--- src/app/api/core/utils/withErrorHandler.ts
+++ src/app/api/core/utils/withErrorHandler.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 import APIError from '@api/core/exceptions/api'
+import { ZodError, ZodIssue } from 'zod'
+import { CopilotApiError } from '@/types/CopilotApiError'
 
 type RequestHandler = (req: NextRequest, params: any) => Promise<NextResponse>
 
@@ -7,15 +9,21 @@ export const withErrorHandler = (handler: RequestHandler): RequestHandler => {
   return async (req: NextRequest, params: any) => {
     try {
       return await handler(req, params)
-    } catch (err) {
-      console.error(err)
-      let error = 'Something went wrong'
+    } catch (error) {
+      console.error(error)
+
       let status = 500
-      if (err instanceof APIError) {
-        error = err.message
-        status = err.status
+      let message: string | ZodIssue[] = 'Something went wrong'
+
+      if (error instanceof ZodError) {
+        status = 400
+        message = error.issues
+      } else if (error instanceof CopilotApiError) {
+        status = error.status || status
+        message = error.body.message || message
       }
-      return NextResponse.json({ error }, { status })
+
+      return NextResponse.json({ error: message }, { status })
     }
   }
 }
--- src/types/CopilotApiError.ts
+++ src/types/CopilotApiError.ts
@@ -1,6 +1,11 @@
-export declare class CopilotApiError extends Error {
+export class CopilotApiError extends Error {
   readonly status: number
   readonly body: {
     message: string
+  }
+  constructor(status: number, body: { message: string }) {
+    super(body.message)
+    this.status = status
+    this.body = body
   }
 }
