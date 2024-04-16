d44d5de1b69f963ca7095b0c8681c4692adaafde
Rojan Rajbhandari <rrojan20@gmail.com>
2024-04-16 19:27:32 +0545

    feat: handle same name workflow state keys

--- src/app/api/workflow-states/workflowStates.service.ts
+++ src/app/api/workflow-states/workflowStates.service.ts
@@ -1,7 +1,7 @@
 import { BaseService } from '@api/core/services/base.service'
 import { CreateWorkflowStateRequest } from '@/types/dto/workflowStates.dto'
 import { Resource } from '@api/core/types/api'
-import { toCamelCase } from '@/utils/string'
+import { generateRandomString, toCamelCase } from '@/utils/string'
 import { PoliciesService } from '@api/core/services/policies.service'
 
 class WorkflowStatesService extends BaseService {
@@ -20,10 +20,20 @@ class WorkflowStatesService extends BaseService {
     return await this.db.workflowState.create({
       data: {
         ...data,
-        key: toCamelCase(data.name),
+        key: await this.generateKey(data.name),
         workspaceId: this.user.workspaceId,
       },
     })
+  }
+
+  private async generateKey(name: string) {
+    let key = toCamelCase(name)
+    // If a key matching existing record exists, append a random
+    // string since we have UQ_WorkflowState_workspaceId_key constraint
+    const matchingKeyRecord = await this.db.workflowState.findFirst({ where: { key } })
+    if (matchingKeyRecord) key += `-${generateRandomString(3)}`
+
+    return key
   }
 }
 
