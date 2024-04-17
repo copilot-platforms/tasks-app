924c1a111960ddd440e706944b98a458e82d9bd4
Rojan Rajbhandari <rrojan20@gmail.com>
2024-04-17 17:02:00 +0545

    chore: use new `authenticate` util

--- src/app/api/users/users.controller.ts
+++ src/app/api/users/users.controller.ts
@@ -1,9 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server'
-import UsersService from './users.service'
-import AuthService from '../core/services/auth.service'
+import UsersService from '@api/users/users.service'
+import authenticate from '@api/core/utils/authenticate'
 
 export const getUsers = async (req: NextRequest) => {
-  const user = await AuthService.authenticate(req)
+  const user = await authenticate(req)
 
   const usersService = new UsersService(user)
   const users = await usersService.getGroupedUsers()
