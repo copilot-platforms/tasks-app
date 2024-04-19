101fa9adca56215d046159d6331a59443c3ed313
aatbip <anantabipal2010@gmail.com>
2024-04-19 15:34:21 +0545

    add: getAllWorkflowStates request

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
 import { DndWrapper } from '@/hoc/DndWrapper'
 import { TaskBoard } from './ui/TaskBoard'
 import { Header } from '@/components/layouts/Header'
+import { CreateWorkflowStateRequest } from '@/types/dto/workflowStates.dto'
+import { apiUrl } from '@/config'
 
-async function getAllWorkflowStates() {}
+async function getAllWorkflowStates(token: string): Promise<CreateWorkflowStateRequest[]> {
+  const res = await fetch(`${apiUrl}/api/workflow-states?token=${token}`)
+
+  const data = await res.json()
+
+  return data.workflowStates
+}
 
 export default async function Main({ searchParams }: { searchParams: { token: string } }) {
   const token = searchParams.token
@@ -10,6 +18,10 @@ export default async function Main({ searchParams }: { searchParams: { token: st
   if (!token) {
     throw new Error('Please pass the token!')
   }
+
+  const workflowStates = await getAllWorkflowStates(token)
+
+  console.log(workflowStates)
 
   return (
     <>
