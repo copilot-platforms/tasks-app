521b887ed0bbee0be4337a3ece10c004b50ebc86
Rojan Rajbhandari <rrojan20@gmail.com>
2024-04-22 21:09:32 +0545

    refactor: reduce scope of PR to not include policies or resource types

--- src/app/api/core/types/user.ts
+++ src/app/api/core/types/user.ts
@@ -1,15 +1,4 @@
 /**
- * UserAction holds every permitted action that a user is allowed to perform on a given `Resource`
- */
-export enum UserAction {
-  All = 'all',
-  Read = 'read',
-  Create = 'create',
-  Update = 'update',
-  Delete = 'delete',
-}
-
-/**
  * User roles as defined by Copilot
  */
 export enum UserRole {
