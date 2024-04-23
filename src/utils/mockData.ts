4654cd2cc7c83d3fdb6a3850e6a0f68f57313338
aatbip <anantabipal2010@gmail.com>
2024-04-23 17:54:53 +0545

    chore: removed unwanted data from mockData.ts file

--- src/utils/mockData.ts
+++ src/utils/mockData.ts
@@ -1,32 +1,3 @@
-//These are mock data for the sake of UI development. It will no
-//longer be of any use and this file will
-//be removed after API integration. Data should be dynamic, right?
-
-import { IAssignee } from '@/types/interfaces'
-
-//This is mock data which will be replaced after API integration.
-export const assignee: IAssignee[] = [
-  {
-    type: '',
-    name: 'No Assignee',
-  },
-  {
-    img: 'https://avatar.iran.liara.run/public/1',
-    type: 'Internal users',
-    name: 'Jacob Jones',
-  },
-  {
-    img: 'https://avatar.iran.liara.run/public/2',
-    type: 'Internal users',
-    name: 'Wade Warren',
-  },
-  {
-    img: 'https://avatar.iran.liara.run/public/3',
-    type: 'Clients',
-    name: 'Brroklyn Simmons',
-  },
-]
-
 export const taskDetail = {
   title: 'my new task',
   detail: '',
