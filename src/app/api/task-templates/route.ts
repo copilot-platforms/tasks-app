dbf3bbc152c439296c37fdf4ff51fa9db7fe78bd
Rojan Rajbhandari <rrojan20@gmail.com>
2024-05-03 15:49:14 +0545

    feat: add templates CRUD endpoint

--- src/app/api/task-templates/route.ts
+++
@@ -1,5 +0,0 @@
-import { withErrorHandler } from '@api/core/utils/withErrorHandler'
-import { getViewSetting, updateViewSetting } from '@api/view-settings/viewSettings.controller'
-
-export const GET = withErrorHandler(getViewSetting)
-export const PATCH = withErrorHandler(updateViewSetting)
--- src/app/api/tasks/templates/templates.service.ts
+++ src/app/api/tasks/templates/templates.service.ts
@@ -1,9 +1,11 @@
+import { CreateTemplateRequest, UpdateTemplateRequest } from '@/types/dto/templates.dto'
 import { BaseService } from '@api/core/services/base.service'
 import { PoliciesService } from '@api/core/services/policies.service'
 import { Resource } from '@api/core/types/api'
 import { UserAction } from '@api/core/types/user'
+import { z } from 'zod'
 
-export class TaskTemplatesService extends BaseService {
+export class TemplatesService extends BaseService {
   async getTaskTemplates() {
     const policyGate = new PoliciesService(this.user)
     policyGate.authorize(UserAction.Read, Resource.TaskTemplates)
@@ -24,33 +26,38 @@ export class TaskTemplatesService extends BaseService {
     return templates
   }
 
-  async createTaskTemplate() {
+  async createTaskTemplate(payload: CreateTemplateRequest) {
     const policyGate = new PoliciesService(this.user)
     policyGate.authorize(UserAction.Create, Resource.TaskTemplates)
 
-    let templates = await this.db.taskTemplate.findMany({
-      where: { workspaceId: this.user.workspaceId },
+    let templates = await this.db.taskTemplate.create({
+      data: {
+        ...payload,
+        workspaceId: this.user.workspaceId,
+        createdBy: z.string().parse(this.user.internalUserId),
+      },
     })
     return templates
   }
 
-  async updateTaskTemplate() {
+  async updateTaskTemplate(id: string, payload: UpdateTemplateRequest) {
     const policyGate = new PoliciesService(this.user)
     policyGate.authorize(UserAction.Update, Resource.TaskTemplates)
 
-    let templates = await this.db.taskTemplate.findMany({
-      where: { workspaceId: this.user.workspaceId },
+    let template = await this.db.taskTemplate.update({
+      where: { id, workspaceId: this.user.workspaceId },
+      data: payload,
     })
-    return templates
+    return template
   }
 
-  async deleteTaskTemplate() {
+  async deleteTaskTemplate(id: string) {
     const policyGate = new PoliciesService(this.user)
     policyGate.authorize(UserAction.Delete, Resource.TaskTemplates)
 
-    let templates = await this.db.taskTemplate.findMany({
-      where: { workspaceId: this.user.workspaceId },
+    let template = await this.db.taskTemplate.delete({
+      where: { id, workspaceId: this.user.workspaceId },
     })
-    return templates
+    return template
   }
 }
