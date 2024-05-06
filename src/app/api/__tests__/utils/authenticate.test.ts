b1e494f8fc96d8ece2695c3127046fdf7f5c0f1f
Rojan Rajbhandari <rrojan20@gmail.com>
2024-05-06 21:28:18 +0545

    test: add tests for authenticate util

---
+++ src/app/api/__tests__/utils/authenticate.test.ts
@@ -0,0 +1,51 @@
+import authenticate from '@api/core/utils/authenticate'
+import { buildNextRequest } from '@api/core/utils/testUtils'
+import httpStatus from 'http-status'
+import { CopilotApiError } from '@/types/CopilotApiError'
+import { mockTokenPayloads } from '@api/__mocks__/mockData'
+import { mockCopilotAPI } from '@api/__mocks__/CopilotAPI.mock'
+
+jest.mock('@/utils/CopilotAPI', () => ({
+  CopilotAPI: jest.fn().mockImplementation((token: string) => mockCopilotAPI(token)),
+}))
+
+describe('authenticate util', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('authenticates a valid IU token', async () => {
+    const req = buildNextRequest(`/?token=iu-token`)
+    const user = await authenticate(req)
+    expect(user.internalUserId).toBe(mockTokenPayloads.internalUser.internalUserId)
+  })
+
+  it('authenticates a valid client token', async () => {
+    const req = buildNextRequest(`/?token=client-token`)
+    const user = await authenticate(req)
+    expect(user.clientId).toBe(mockTokenPayloads.client.clientId)
+  })
+
+  it('throws APIError if token is not provided', async () => {
+    const req = buildNextRequest(`/?token=`)
+    try {
+      await authenticate(req)
+      fail('Expected authenticate function to throw an error, but it did not')
+    } catch (error: unknown) {
+      expect(error).toBeInstanceOf(CopilotApiError)
+      expect((error as CopilotApiError).status).toBe(httpStatus.UNAUTHORIZED)
+      expect((error as Error).message).toBe('Failed to authenticate token')
+    }
+  })
+
+  it('throws CopilotApiError if token cannot be authenticated', async () => {
+    const req = buildNextRequest(`/?token=invalid-token`)
+    try {
+      await authenticate(req)
+      fail('Expected authenticate function to throw an error, but it did not')
+    } catch (error: unknown) {
+      expect(error).toBeInstanceOf(CopilotApiError)
+      expect((error as Error).message).toBe('Failed to authenticate token')
+    }
+  })
+})
