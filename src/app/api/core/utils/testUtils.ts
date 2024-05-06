ce862c6db0ef8ca6b782a552adce907f64db3b4c
Rojan Rajbhandari <rrojan20@gmail.com>
2024-05-06 22:28:41 +0545

    fix: read base url from VERCEL_URL

--- src/app/api/core/utils/testUtils.ts
+++ src/app/api/core/utils/testUtils.ts
@@ -1,3 +1,3 @@
 import { NextRequest } from 'next/server'
 
-export const buildNextRequest = (url: string) => new NextRequest(new Request('http://localhost:3000' + url), {})
+export const buildNextRequest = (url: string) => new NextRequest(new Request(process.env.VERCEL_URL + url), {})
