db339e23c30b35102ad07f89acafa0592bb91a2e
aatbip <anantabipal2010@gmail.com>
2024-05-13 20:04:56 +0545

    reverted commit

--- src/app/detail/ui/TaskEditor.tsx
+++ src/app/detail/ui/TaskEditor.tsx
@@ -10,8 +10,6 @@ import { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { ConfirmDeleteUI } from '@/components/layouts/ConfirmDeleteUI'
 import store from '@/redux/store'
-import { NotionLike } from 'notion-like'
-import { upload } from '@vercel/blob/client'
 
 type Attachment = {
   name: string
@@ -60,16 +58,28 @@ export const TaskEditor = ({ title, detail, attachment, isEditable, updateTaskDe
         />
       </Stack>
       <Box>
-        <NotionLike
-          uploadFn={async (file, tiptapEditorUtils) => {
-            const newBlob = await upload(file.name, file, {
-              access: 'public',
-              handleUploadUrl: '/api/upload',
-            })
-            tiptapEditorUtils.setImage(newBlob.url as string)
+        <StyledTextField
+          type="text"
+          placeholder="Add description..."
+          multiline
+          borderLess
+          minRows={8}
+          sx={{
+            width: '100%',
+            '& .MuiInputBase-input': {
+              fontSize: '16px',
+              lineHeight: '24px',
+              color: (theme) => theme.color.gray[500],
+              fontWeight: 400,
+            },
           }}
-          content={detail}
-          getContent={(content) => setUpdateDetail(content)}
+          value={updateDetail}
+          onChange={(e) => setUpdateDetail(e.target.value)}
+          InputProps={{ readOnly: !isEditable }}
+          disabled={!isEditable}
+          onBlur={() => {
+            updateTaskDetail(updateTitle, updateDetail)
+          }}
         />
       </Box>
       <Stack direction="row" columnGap={3} mt={3}>
