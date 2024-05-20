7805d68207bcdbcd32a63a3b41d696e9af5d6793
aatbip <anantabipal2010@gmail.com>
2024-05-20 19:25:33 +0545

    feat: adds responsiveness for filter bars

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -19,28 +19,30 @@ export const Header = ({ showCreateTaskButton }: { showCreateTaskButton: boolean
         <Stack direction="row" alignItems="center" justifyContent="space-between">
           <Typography variant="lg">Tasks</Typography>
           {showCreateTaskButton && (
-            <Box
-              sx={{
-                display: { xs: 'none', sm: 'block' },
-              }}
-            >
-              <PrimaryBtn
-                startIcon={<Add />}
-                buttonText="New Task"
-                handleClick={() => {
-                  store.dispatch(setShowModal())
+            <>
+              <Box
+                sx={{
+                  display: { xs: 'none', sm: 'block' },
                 }}
-              />
-            </Box>
+              >
+                <PrimaryBtn
+                  startIcon={<Add />}
+                  buttonText="New Task"
+                  handleClick={() => {
+                    store.dispatch(setShowModal())
+                  }}
+                />
+              </Box>
+              <Box sx={{ display: { xs: 'block', sm: 'none' } }}>
+                <IconBtn
+                  handleClick={() => {
+                    store.dispatch(setShowModal())
+                  }}
+                  icon={<Add sx={{ color: '#fff' }} />}
+                />
+              </Box>
+            </>
           )}
-          <Box sx={{ display: { xs: 'block', sm: 'none' } }}>
-            <IconBtn
-              handleClick={() => {
-                store.dispatch(setShowModal())
-              }}
-              icon={<Add sx={{ color: '#fff' }} />}
-            />
-          </Box>
         </Stack>
       </AppMargin>
     </Box>
