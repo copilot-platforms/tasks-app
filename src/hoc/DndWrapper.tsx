2c4d4f3e3c7c3bb031b9a7ee4655de158193e269
aatbip <anantabipal2010@gmail.com>
2024-05-20 19:30:10 +0545

    feat: adds TouchBackend for mobile DnD

--- src/hoc/DndWrapper.tsx
+++ src/hoc/DndWrapper.tsx
@@ -1,9 +1,21 @@
 'use client'
 
+import { useMediaQuery } from '@mui/material'
 import { ReactNode } from 'react'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
+import { TouchBackend } from 'react-dnd-touch-backend'
 
 export const DndWrapper = ({ children }: { children: ReactNode }) => {
-  return <DndProvider backend={HTML5Backend}>{children}</DndProvider>
+  const matches = useMediaQuery('(max-width:600px)')
+
+  const options = {
+    scrollAngleRanges: [{ start: 300 }, { end: 60 }, { start: 120, end: 240 }],
+  }
+
+  return (
+    <DndProvider backend={matches ? TouchBackend : HTML5Backend} options={options}>
+      {children}
+    </DndProvider>
+  )
 }
