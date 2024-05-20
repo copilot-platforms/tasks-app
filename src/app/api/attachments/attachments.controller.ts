3eb533eb28a85c16771544a70bfcb27116dda7b6
Arpan Dhakal <arpandhakal20@gmail.com>
2024-05-20 14:47:02 +0545

    tryfix: test

--- src/app/api/attachments/attachments.controller.ts
+++ src/app/api/attachments/attachments.controller.ts
@@ -13,9 +13,7 @@ import APIError from '../core/exceptions/api'
 
 export const createAttachment = async (req: NextRequest) => {
   const user = await authenticate(req)
-  console.log(req)
   const body = CreateAttachmentRequestSchema.parse(await req.json())
-  console.log(body)
   const attachmentsService = new AttachmentsService(user)
   const newAttachment = await attachmentsService.createAttachments(body)
   return NextResponse.json({ newAttachment }, { status: httpStatus.CREATED })
