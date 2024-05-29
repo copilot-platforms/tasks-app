72c7e5ef8b9452e01cce304ead41352e7ba19073
aatbip <anantabipal2010@gmail.com>
2024-05-29 22:20:17 +0545

    test:middleware implementation

---
+++ src/middleware.ts
@@ -0,0 +1,7 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  return NextResponse.json({ message: 'hello' })
+}
