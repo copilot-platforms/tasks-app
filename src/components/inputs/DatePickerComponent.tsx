78f17f6ed9dcb68509f4bc67ce6d2c815c6242fc
Ananta Bipal <anantabipal2010@gmail.com>
2024-05-31 17:07:09 +0545

    OUT 399 When IU clicks on Due Date value, they should see the calendar (#57)
    * hot fix
    
    * fix:inproved datapicker UI

--- src/components/inputs/DatePickerComponent.tsx
+++ src/components/inputs/DatePickerComponent.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { DatePicker } from '@mui/x-date-pickers/DatePicker'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import dayjs, { Dayjs } from 'dayjs'
 import { CalenderIcon } from '@/icons'
 import { IsoDate } from '@/types/dto/tasks.dto'
+import { Box, Popper, Stack, Typography } from '@mui/material'
 
 interface Prop {
   getDate: (value: string) => void
@@ -18,40 +19,78 @@ export const DatePickerComponent = ({ getDate, dateValue }: Prop) => {
     return date ? date.format('MMM DD, YYYY') : '' // Format the date as "Mar 08, 2024"
   }
 
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(anchorEl ? null : event.currentTarget)
+  }
+
+  const open = Boolean(anchorEl)
+  const id = open ? 'calender-element' : undefined
+
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DatePicker
-        value={value}
-        onChange={(newValue) => {
-          getDate(formatDate(newValue))
-          setValue(newValue)
-        }}
-        sx={{
-          '& .MuiOutlinedInput-input': {
-            padding: 2,
-            paddingLeft: 2,
-            fontSize: '14px',
-          },
-          '& .MuiOutlinedInput-root': {
-            '& fieldset': {
-              borderColor: (theme) => theme.color.gray[400],
+      <Stack
+        direction="row"
+        alignItems="center"
+        columnGap="7px"
+        onClick={handleClick}
+        aria-describedby={id}
+        sx={{ cursor: 'pointer' }}
+      >
+        <Box>
+          <CalenderIcon />
+        </Box>
+        <Typography variant="bodyMd">{formatDate(value)}</Typography>
+      </Stack>
+      <Popper
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        placement="bottom-end"
+        modifiers={[
+          {
+            name: 'offset',
+            options: {
+              offset: [0, 4],
             },
-            '&:hover fieldset': {
-              borderColor: (theme) => theme.color.gray[400],
-            },
-            '&.Mui-focused fieldset': {
-              borderColor: (theme) => theme.color.gray[400],
-            },
           },
-        }}
-        slotProps={{
-          inputAdornment: {
-            position: 'start',
-          },
-        }}
-        slots={{ openPickerIcon: CalenderIcon }}
-        format={dateValue ? 'MMM DD, YYYY' : ''}
-      />
+        ]}
+      >
+        <DatePicker
+          value={value}
+          open={open}
+          onClose={() => setAnchorEl(null)}
+          onChange={(newValue) => {
+            getDate(formatDate(newValue))
+            setValue(newValue)
+          }}
+          sx={{
+            '& .MuiOutlinedInput-input': {
+              padding: 2,
+              paddingLeft: 2,
+              fontSize: '14px',
+              display: 'none',
+            },
+            '& .MuiButtonBase-root': {
+              display: 'none',
+            },
+            '& .MuiOutlinedInput-root': {
+              display: open ? 'block' : 'none',
+              '& fieldset': {
+                borderColor: (theme) => theme.color.gray[400],
+              },
+              '&:hover fieldset': {
+                borderColor: (theme) => theme.color.gray[400],
+              },
+              '&.Mui-focused fieldset': {
+                borderColor: (theme) => theme.color.gray[400],
+              },
+            },
+          }}
+          format={dateValue ? 'MMM DD, YYYY' : ''}
+        />
+      </Popper>
     </LocalizationProvider>
   )
 }
