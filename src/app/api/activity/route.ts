443c94ca8fb44eeda731fbd5ebc5f9b52ac9fd8f
aatbip <anantabipal2010@gmail.com>
2024-05-31 12:49:29 +0545

    test

--- src/app/api/activity/route.ts
+++
@@ -1,6 +0,0 @@
-import DBClient from '@/lib/db'
-import { PrismaClient } from '@prisma/client'
-import { NextRequest } from 'next/server'
-
-const db: PrismaClient = DBClient.getInstance()
-export const GET = (req: NextRequest) => {}
