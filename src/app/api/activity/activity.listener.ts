096fd5098c19dad04a762cb1c5989b03c7768190
aatbip <anantabipal2010@gmail.com>
2024-06-05 16:54:58 +0545

    chore:adds absolute imports

--- src/app/api/activity/activity.listener.ts
+++ src/app/api/activity/activity.listener.ts
@@ -1,8 +1,8 @@
 import { EventEmitter } from 'node:events'
-import User from '../core/models/User.model'
+import User from '@/app/api/core/models/User.model'
 import { UpdateTaskRequest } from '@/types/dto/tasks.dto'
 import { Task } from '@prisma/client'
-import { ActivityLogger } from './activity.service'
+import { ActivityLogger } from '@/app/api/activity/activity.service'
 
 export const activityEvents = new EventEmitter()
 
