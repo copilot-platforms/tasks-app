66cc35c59add1165c0a4a7aa5350eb3c0165bc69
aatbip <anantabipal2010@gmail.com>
2024-06-21 14:41:03 +0545

    chore:adds new migration for Label schema

--- prisma/migrations/20240621085459_adds_labels_schema/migration.sql
+++ prisma/migrations/20240621085459_adds_labels_schema/migration.sql
@@ -8,5 +8,20 @@
 -- AlterTable
 ALTER TABLE "Tasks" ADD COLUMN     "label" TEXT NOT NULL;
 
+-- CreateTable
+CREATE TABLE "Labels" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "label" TEXT NOT NULL,
+    "labelledEntity" TEXT NOT NULL,
+    "createdAt" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMPTZ NOT NULL,
+    "deletedAt" TIMESTAMPTZ,
+
+    CONSTRAINT "Labels_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Labels_label_key" ON "Labels"("label");
+
 -- CreateIndex
 CREATE UNIQUE INDEX "Tasks_label_key" ON "Tasks"("label");
