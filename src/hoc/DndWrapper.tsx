19d09b77afcea0bb8eb30b98c8313027235b1eb3
aatbip <anantabipal2010@gmail.com>
2024-06-26 19:00:30 +0545

    fix:removed ignored scroll options

--- src/hoc/DndWrapper.tsx
+++ src/hoc/DndWrapper.tsx
@@ -1,13 +1,24 @@
 'use client'
 
-import React, { ReactNode } from 'react'
+import React, { ReactNode, useEffect, useState } from 'react'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
 import { TouchBackend } from 'react-dnd-touch-backend'
 import { useMediaQuery } from '@mui/material'
 
 export const DndWrapper = ({ children }: { children: ReactNode }) => {
-  const matches = useMediaQuery('(max-width:600px)')
+  const [screenType, setScreenType] = useState<'mobile' | 'nonMobile' | undefined>(undefined)
 
-  return <DndProvider backend={matches ? TouchBackend : HTML5Backend}>{children}</DndProvider>
+  const isMobileScreen = useMediaQuery('(max-width:600px)')
+  useEffect(() => {
+    if (isMobileScreen) {
+      setScreenType('mobile')
+    } else {
+      setScreenType('nonMobile')
+    }
+  }, [isMobileScreen])
+
+  if (!screenType) return null
+
+  return <DndProvider backend={screenType === 'mobile' ? TouchBackend : HTML5Backend}>{children}</DndProvider>
 }
