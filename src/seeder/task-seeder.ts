2f4ca19dbf7fb35ba5455c128b06c8ec623ce9e7
aatbip <anantabipal2010@gmail.com>
2024-06-27 14:37:30 +0545

    feat:adds seeder for tasks

--- src/seeder/task-seeder.ts
+++ src/seeder/task-seeder.ts
@@ -4,8 +4,7 @@ import { WorkflowStateResponse } from '@/types/dto/workflowStates.dto'
 
 const apiUrl = 'http://localhost:3000'
 
-const token =
-  'd4d18a74fdbfa04d57e87525d9d6b0c34170d872a6958803db390e49e532c0c45f628d8b1079a60f7b3111af0056b7cfeaa9db33a6ff1e3ab18985d56824b2cda5e1ca007620f46499a6c6a97b20ba629119c08ff6d9059e24ce3d04ea6ff9bd06ec577f31b9fe9a876b316d61f70ac6'
+const token = ''
 
 async function getAllWorkflowStates(): Promise<WorkflowStateResponse[]> {
   const res = await fetch(`${apiUrl}/api/workflow-states?token=${token}`)
@@ -39,6 +38,9 @@ async function seedTasks(count: number, workflowStateId: string) {
 }
 
 async function seedAllTasks(count: number) {
+  if (!token) {
+    throw new Error('Please pass a token!')
+  }
   const workflowStates = await getAllWorkflowStates()
   for (let i = 0; i < workflowStates.length; i++) {
     const workflowStateId = workflowStates[i].id
