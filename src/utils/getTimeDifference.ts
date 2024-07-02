a85709b4d0119c0e958b6cdedbd44915b078783f
Arpan Dhakal <arpandhakal20@gmail.com>
2024-07-02 11:40:38 +0545

    fix : requested changes

--- src/utils/getTimeDifference.ts
+++ src/utils/getTimeDifference.ts
@@ -3,4 +3,4 @@ import relativeTime from 'dayjs/plugin/relativeTime'
 
 dayjs.extend(relativeTime)
 
-export const getTimeDifference = (createdAt: string): string => dayjs().to(dayjs(createdAt))
+export const getTimeDifference = (createdAt: string): string => dayjs().to(dayjs(createdAt)) //computes time difference to readable time difference between the current time and provided time. The dayjs extentention converts the difference to results like an hour ago, a day ago, 3 days ago and so on.
