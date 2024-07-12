faacb67f44c80c45bdf66dbee6799aa491eef436
Rojan Rajbhandari <rrojan20@gmail.com>
2024-07-12 14:17:19 +0545

    refactor(OUT-556): lint fix code

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -3,14 +3,14 @@
 // Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
-  dsn: "https://a9e04dcf55b4aedef5aa8dbaf50ca1ee@o4504254375133184.ingest.us.sentry.io/4507583195578368",
+  dsn: 'https://a9e04dcf55b4aedef5aa8dbaf50ca1ee@o4504254375133184.ingest.us.sentry.io/4507583195578368',
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
-});
+})
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -2,10 +2,10 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
-  dsn: "https://a9e04dcf55b4aedef5aa8dbaf50ca1ee@o4504254375133184.ingest.us.sentry.io/4507583195578368",
+  dsn: 'https://a9e04dcf55b4aedef5aa8dbaf50ca1ee@o4504254375133184.ingest.us.sentry.io/4507583195578368',
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
@@ -15,5 +15,4 @@ Sentry.init({
 
   // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
   // spotlight: process.env.NODE_ENV === 'development',
-  
-});
+})
