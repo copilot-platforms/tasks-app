d3413f919ceae125b3b85e94cf941e792e0e8156
Rojan Rajbhandari <rrojan20@gmail.com>
2024-07-16 18:56:22 +0545

    feat: add util to filter only accessible users

--- src/utils/assignee.ts
+++ src/utils/assignee.ts
@@ -1,6 +1,8 @@
-import { IAssigneeCombined } from '@/types/interfaces'
+import { FilterByOptions, IAssigneeCombined, IIus } from '@/types/interfaces'
 import { truncateText } from './truncateText'
 import { TruncateMaxNumber } from '@/types/constants'
+import { z } from 'zod'
+import { InternalUsers } from '@/types/common'
 
 export const isAssigneeTextMatching = (newInputValue: string, assigneeValue: IAssigneeCombined): boolean => {
   const truncate = (newInputValue: string) => truncateText(newInputValue, TruncateMaxNumber.SELECTOR)
@@ -8,4 +10,22 @@ export const isAssigneeTextMatching = (newInputValue: string, assigneeValue: IAs
     truncate(newInputValue) === truncate(`${assigneeValue?.givenName} ${assigneeValue?.familyName}`.trim()) ||
     truncate(newInputValue) === truncate(assigneeValue?.name?.trim() || '')
   )
+}
+
+export const getAccessibleUsersList = (
+  users: IAssigneeCombined[],
+  currentInternalUser?: InternalUsers,
+): IAssigneeCombined[] => {
+  return currentInternalUser?.isClientAccessLimited
+    ? users.filter((user) => {
+        if (user.type === FilterByOptions.IUS) {
+          return true
+        }
+
+        const companyId = user.type === FilterByOptions.CLIENT ? z.string().parse(user.companyId) : user.id
+        // If currently logged in IU has restricted company access, make sure to filter out companies that aren't in its company access list
+        const companyAccessList = currentInternalUser.companyAccessList || [] // There seems to be data where access is restricted but companyAccessList is still null
+        return companyAccessList.includes(companyId)
+      })
+    : users
 }
