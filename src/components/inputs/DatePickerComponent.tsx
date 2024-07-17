af59fcf86c34c60d23086339e91727f6f2fd8d9e
Arpan Dhakal <arpandhakal20@gmail.com>
2024-07-17 15:26:09 +0545

    fix : comment changed

--- src/components/inputs/DatePickerComponent.tsx
+++ src/components/inputs/DatePickerComponent.tsx
@@ -19,7 +19,7 @@ export const DatePickerComponent = ({ getDate, dateValue, disabled, isButton = f
   const [value, setValue] = React.useState<Dayjs | null>(dateValue ? dayjs(dateValue) : null)
 
   const formatDate = (date: Dayjs | null) => {
-    return date ? date.format('MMMM DD, YYYY') : '' // Format the date as "Mar 08, 2024"
+    return date ? date.format('MMMM DD, YYYY') : '' // Format the date as "March 08, 2024"
   }
 
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
