6b26bda48bf342a3a4fb3f9ccaf15b5ea6d9f62d
aatbip <anantabipal2010@gmail.com>
2024-07-22 15:52:33 +0545

    fix: fetch token payload on client side

--- src/app/client/page.tsx
+++ src/app/client/page.tsx
@@ -10,6 +10,8 @@ import { addTypeToAssignee } from '@/utils/addTypeToAssignee'
 import { ClientTaskBoard } from './ui/ClientTaskBoard'
 import { completeTask } from '@/app/client/actions'
 import { RealTime } from '@/hoc/RealTime'
+import { CopilotAPI } from '@/utils/CopilotAPI'
+import { Token, TokenSchema } from '@/types/common'
 
 async function getAllWorkflowStates(token: string): Promise<WorkflowStateResponse[]> {
   const res = await fetch(`${apiUrl}/api/workflow-states?token=${token}`, {
@@ -38,17 +40,30 @@ async function getAssigneeList(token: string): Promise<IAssignee> {
   return data.clients
 }
 
+async function getTokenPayload(token: string): Promise<Token> {
+  const copilotClient = new CopilotAPI(token)
+  const payload = TokenSchema.parse(await copilotClient.getTokenPayload())
+  return payload as Token
+}
+
 export default async function ClientPage({ searchParams }: { searchParams: { token: string } }) {
   const token = searchParams.token
 
-  const [workflowStates, tasks, assignee] = await Promise.all([
+  const [workflowStates, tasks, assignee, tokenPayload] = await Promise.all([
     await getAllWorkflowStates(token),
     await getAllTasks(token),
     addTypeToAssignee(await getAssigneeList(token)),
+    getTokenPayload(token),
   ])
   return (
     <>
-      <ClientSideStateUpdate workflowStates={workflowStates} tasks={tasks} token={token} assignee={assignee}>
+      <ClientSideStateUpdate
+        workflowStates={workflowStates}
+        tasks={tasks}
+        token={token}
+        assignee={assignee}
+        tokenPayload={tokenPayload}
+      >
         <RealTime>
           <ClientTaskBoard
             completeTask={async (taskId) => {
