3b5fc3e259ff50a4eba89ff577de0b0686eb01f7
Rojan Rajbhandari <rrojan20@gmail.com>
2024-07-25 15:28:13 +0545

    chore(lib): move supabase privileges query to seperate lib cmd

---
+++ src/lib/supabase-privilege/index.js
@@ -0,0 +1,34 @@
+/**
+ * `supabase-privilege` helps run the `grant-privileges.sql` query in this directory
+ *
+ * We are granting privileges to required roles this way, because Supabase for some reason flushes all permissions
+ * after each migrate.
+ */
+
+const { PrismaClient } = require('@prisma/client')
+const fs = require('fs')
+const path = require('path')
+
+const prisma = new PrismaClient()
+
+async function runSqlScript() {
+  const sqlPath = path.resolve(__dirname, 'grant-privileges.sql')
+  const sql = fs.readFileSync(sqlPath, 'utf8')
+  const statements = sql
+    .split(';')
+    .map((statement) => statement.trim())
+    .filter((statement) => statement)
+
+  try {
+    for (const statement of statements) {
+      await prisma.$executeRawUnsafe(statement)
+    }
+    console.log('🔥 grant-privileges executed successfully 🔥')
+  } catch (error) {
+    console.error('Error executing grant-privileges:', error)
+  } finally {
+    await prisma.$disconnect()
+  }
+}
+
+runSqlScript()
