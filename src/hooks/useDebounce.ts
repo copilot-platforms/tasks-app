525c3e9b9536998dc2accdcb7c21f553294e4463
aatbip <anantabipal2010@gmail.com>
2024-07-26 22:49:54 +0545

    test:debounce update and realtime test-20

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,72 +1,30 @@
-// import { useRef, useEffect } from 'react'
-
-// type Timer = ReturnType<typeof setTimeout>
-// type SomeFunction = (...args: any[]) => void
-// /**
-//  *
-//  * @param func The original, non debounced function (You can pass any number of args to it)
-//  * @param delay The delay (in ms) for the function to return
-//  * @returns The debounced function, which will run only if the debounced function has not been called in the last (delay) ms
-//  */
-
-// export function useDebounce<Func extends SomeFunction>(func: Func, delay = 500) {
-//   const timer = useRef<Timer | null>(null)
-//   useEffect(() => {
-//     return () => {
-//       if (!timer.current) return
-//       clearTimeout(timer.current)
-//     }
-//   }, [])
-
-//   const debouncedFunction = ((...args: Parameters<Func>) => {
-//     const newTimer = setTimeout(() => {
-//       func(...args)
-//     }, delay)
-//     timer.current && clearTimeout(timer.current)
-//     timer.current = newTimer
-//   }) as Func
-
-//   return debouncedFunction
-// }
-
-import { useRef, useEffect, useCallback } from 'react'
+import { useRef, useEffect } from 'react'
 
 type Timer = ReturnType<typeof setTimeout>
 type SomeFunction = (...args: any[]) => void
-
 /**
  *
  * @param func The original, non debounced function (You can pass any number of args to it)
  * @param delay The delay (in ms) for the function to return
- * @returns The debounced function, which will run periodically while the debounced function has been called in the last (delay) ms
+ * @returns The debounced function, which will run only if the debounced function has not been called in the last (delay) ms
  */
+
 export function useDebounce<Func extends SomeFunction>(func: Func, delay = 500) {
   const timer = useRef<Timer | null>(null)
-  const lastArgs = useRef<Parameters<Func>>()
-
-  const debouncedFunction = useCallback(
-    (...args: Parameters<Func>) => {
-      lastArgs.current = args
-      if (timer.current) clearTimeout(timer.current)
-      timer.current = setTimeout(() => {
-        func(...args)
-      }, delay)
-    },
-    [func, delay],
-  )
-
   useEffect(() => {
-    const interval = setInterval(() => {
-      if (lastArgs.current) {
-        func(...lastArgs.current)
-      }
-    }, delay)
-
     return () => {
-      clearInterval(interval)
-      if (timer.current) clearTimeout(timer.current)
+      if (!timer.current) return
+      clearTimeout(timer.current)
     }
-  }, [func, delay])
+  }, [])
 
-  return debouncedFunction as Func
+  const debouncedFunction = ((...args: Parameters<Func>) => {
+    const newTimer = setTimeout(() => {
+      func(...args)
+    }, delay)
+    timer.current && clearTimeout(timer.current)
+    timer.current = newTimer
+  }) as Func
+
+  return debouncedFunction
 }
