18325c6c2d1c24234a51d8e404c037319fe80b99
aatbip <anantabipal2010@gmail.com>
2024-07-29 23:51:51 +0545

    feat:adds sort by descending for tasks

--- src/utils/sortTask.ts
+++ src/utils/sortTask.ts
@@ -4,6 +4,6 @@ export const sortTaskByDescendingOrder = (tasks: TaskResponse[]) => {
   return tasks.sort((a, b) => {
     const dateA = new Date(a.createdAt).getTime()
     const dateB = new Date(b.createdAt).getTime()
-    return dateA - dateB
+    return dateB - dateA
   })
 }
