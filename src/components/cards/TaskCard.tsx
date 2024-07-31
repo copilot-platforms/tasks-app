30aff7ec4328eb631f8e96fb33a4d59280768b2c
Arpan Dhakal <arpandhakal20@gmail.com>
2024-07-31 14:36:43 +0545

    fix(OUT-674) : fixed width for taskcard and taskcolum according to figma and fixed the click handler scope(width)

--- src/components/cards/TaskCard.tsx
+++ src/components/cards/TaskCard.tsx
@@ -4,7 +4,7 @@ import { selectTaskBoard } from '@/redux/features/taskBoardSlice'
 import { TaskResponse } from '@/types/dto/tasks.dto'
 import { IAssigneeCombined } from '@/types/interfaces'
 import { NoAssignee } from '@/utils/noAssignee'
-import { Stack, Typography, styled } from '@mui/material'
+import { Box, Stack, Typography, styled } from '@mui/material'
 import { useSelector } from 'react-redux'
 import { DueDateLayout } from '@/components/layouts/DueDateLayout'
 import { CopilotAvatar } from '@/components/atoms/CopilotAvatar'
@@ -22,7 +22,7 @@ const TaskCardContainer = styled(Stack)(({ theme }) => ({
     background: theme.color.gray[150],
   },
   cursor: 'pointer',
-  width: '282px',
+  width: '279px',
 }))
 
 interface TaskCardProps {
@@ -36,37 +36,39 @@ export const TaskCard = ({ task, href }: TaskCardProps) => {
   const currentAssignee = assignee.find((el) => el.id === task.assigneeId) ?? NoAssignee
 
   return (
-    <CustomLink href={href}>
-      <TaskCardContainer>
-        <Stack rowGap={1}>
-          <Stack direction="row" justifyContent="space-between">
-            <Stack direction="row" alignItems="center" columnGap={1}>
-              <CopilotAvatar currentAssignee={currentAssignee as IAssigneeCombined} />
-              <Typography
-                variant="sm"
-                fontSize="12px"
-                sx={{
-                  color: (theme) => theme.color.gray[500],
-                  width: '146px',
-                  overflow: 'hidden',
-                  textOverflow: 'ellipsis',
-                  whiteSpace: 'nowrap',
-                }}
-              >
-                {(currentAssignee as IAssigneeCombined).name === 'No assignee'
-                  ? 'No assignee'
-                  : (currentAssignee as IAssigneeCombined)?.name ||
-                    `${(currentAssignee as IAssigneeCombined)?.givenName ?? ''} ${(currentAssignee as IAssigneeCombined)?.familyName ?? ''}`.trim()}
+    <Box sx={{ width: '279px' }}>
+      <CustomLink href={href}>
+        <TaskCardContainer>
+          <Stack rowGap={1}>
+            <Stack direction="row" justifyContent="space-between">
+              <Stack direction="row" alignItems="center" columnGap={1}>
+                <CopilotAvatar currentAssignee={currentAssignee as IAssigneeCombined} />
+                <Typography
+                  variant="sm"
+                  fontSize="12px"
+                  sx={{
+                    color: (theme) => theme.color.gray[500],
+                    width: '146px',
+                    overflow: 'hidden',
+                    textOverflow: 'ellipsis',
+                    whiteSpace: 'nowrap',
+                  }}
+                >
+                  {(currentAssignee as IAssigneeCombined).name === 'No assignee'
+                    ? 'No assignee'
+                    : (currentAssignee as IAssigneeCombined)?.name ||
+                      `${(currentAssignee as IAssigneeCombined)?.givenName ?? ''} ${(currentAssignee as IAssigneeCombined)?.familyName ?? ''}`.trim()}
+                </Typography>
+              </Stack>
+              <Typography variant="bodyXs" fontWeight={400} sx={{ color: (theme) => theme.color.gray[500] }}>
+                {task.label}
               </Typography>
             </Stack>
-            <Typography variant="bodyXs" fontWeight={400} sx={{ color: (theme) => theme.color.gray[500] }}>
-              {task.label}
-            </Typography>
+            <Typography variant="sm">{task.title}</Typography>
+            {task.dueDate && <DueDateLayout dateString={task.dueDate} />}
           </Stack>
-          <Typography variant="sm">{task.title}</Typography>
-          {task.dueDate && <DueDateLayout dateString={task.dueDate} />}
-        </Stack>
-      </TaskCardContainer>
-    </CustomLink>
+        </TaskCardContainer>
+      </CustomLink>
+    </Box>
   )
 }
