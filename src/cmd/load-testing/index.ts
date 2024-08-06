c54cab5a13f02b4bccca445c0be049a87472b2b2
Rojan Rajbhandari <rrojan20@gmail.com>
2024-08-06 20:00:03 +0545

    chore(OUT-548): add final config for load testing

--- src/cmd/load-testing/index.ts
+++ src/cmd/load-testing/index.ts
@@ -1,27 +1,24 @@
 import config from '@cmd/load-testing/load-testing.config.json'
-import LoadTester, { Taskable, TaskableAssigneeType } from '@cmd/load-testing/load-testing.service'
-import { AssigneeType } from '@prisma/client'
+import LoadTester from '@cmd/load-testing/load-testing.service'
 
 /**
     Load testing script for Tasks App
     Add a `LOAD_TESTING_COPILOT_TOKEN` env var with the corresponding workspace's IU token, and let it work its magic.
 
     Configure counts for clients, companies, tasks, etc using the `load-testing.config.json` JSON file
+
+    Criteria:
+      - 5000 clients / companies
+      - 2000 tasks
 */
 export const run = async () => {
-  // Criteria:
-  // - 5000 clients / companies
-  // - 2000 tasks
   const loadTester = new LoadTester()
-  const individualClients = await loadTester.seedClients(config.clients.individual)
-  const { companies, clients: companyClients } = await loadTester.seedCompanyClients(
-    config.companies,
-    config.clients.company,
-  )
+  const individualClients = await loadTester.seedClients(config.individualClients)
+  const { companies, clients: companyClients } = await loadTester.seedCompanyClients(config.companies, config.companyClients)
 
-  await loadTester.seedClientTasks(individualClients, config.tasks)
-  await loadTester.seedCompanyTasks(companies, config.tasks)
-  await loadTester.seedClientTasks(companyClients, config.tasks)
+  await loadTester.seedClientTasks(individualClients, config.countsToAssign.individualClients)
+  await loadTester.seedCompanyTasks(companies, config.countsToAssign.companies)
+  await loadTester.seedClientTasks(companyClients, config.countsToAssign.companyClients)
 }
 
 run()
