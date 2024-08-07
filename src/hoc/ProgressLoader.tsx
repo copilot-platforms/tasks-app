1a4f4747698d8ad78ad19d791eb55de22e154977
aatbip <anantabipal2010@gmail.com>
2024-08-07 19:02:13 +0545

    feat:adds loader for route transitions

--- src/hoc/ProgressLoader.tsx
+++ src/hoc/ProgressLoader.tsx
@@ -1,6 +1,12 @@
 'use client'
 import { ProgressLoader } from 'nextjs-progressloader'
+import { Suspense } from 'react'
 
 export const ProgressLoad = () => {
-  return <ProgressLoader color="#212B36" crawlSpeed={100} height={5} showSpinner={false} />
+  return (
+    <Suspense fallback={null}>
+      {' '}
+      <ProgressLoader color="#212B36" crawlSpeed={100} height={5} />
+    </Suspense>
+  )
 }
