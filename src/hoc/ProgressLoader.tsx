ae1cd1d9bf6fd1c907262748f7aec25e6e598e0b
aatbip <anantabipal2010@gmail.com>
2024-08-07 19:43:00 +0545

    feat:updates loader style

--- src/hoc/ProgressLoader.tsx
+++ src/hoc/ProgressLoader.tsx
@@ -5,8 +5,12 @@ import { Suspense } from 'react'
 export const ProgressLoad = () => {
   return (
     <Suspense fallback={null}>
-      {' '}
-      <ProgressLoader color="#212B36" crawlSpeed={100} height={5} />
+      <ProgressLoader
+        color="#212B36"
+        crawlSpeed={100}
+        height={5}
+        template='<div class="bar" role="bar"><div class="peg"></div></div>'
+      />
     </Suspense>
   )
 }
