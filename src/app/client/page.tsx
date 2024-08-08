f944ffbbf395ff092ae1d5c901006905938eed0c
aatbip <anantabipal2010@gmail.com>
2024-08-08 18:40:35 +0545

    feat:fetch requests optimization test - 3

--- src/app/client/page.tsx
+++ src/app/client/page.tsx
@@ -1,17 +1,36 @@
 export const fetchCache = 'force-no-store'
 
+import { apiUrl } from '@/config'
+import { ClientSideStateUpdate } from '@/hoc/ClientSideStateUpdate'
+import { IAssignee } from '@/types/interfaces'
+import { addTypeToAssignee } from '@/utils/addTypeToAssignee'
 import { ClientTaskBoard } from './ui/ClientTaskBoard'
 import { completeTask } from '@/app/client/actions'
+import { MAX_FETCH_ASSIGNEE_COUNT } from '@/constants/users'
+
+async function getAssigneeList(token: string): Promise<IAssignee> {
+  const res = await fetch(`${apiUrl}/api/users/client?token=${token}&limit=${MAX_FETCH_ASSIGNEE_COUNT}`, {
+    next: { tags: ['getAssigneeList'] },
+  })
+  const data = await res.json()
+  return data.clients
+}
 
 export default async function ClientPage({ searchParams }: { searchParams: { token: string } }) {
   const token = searchParams.token
 
+  const [assignee] = await Promise.all([addTypeToAssignee(await getAssigneeList(token))])
+
   return (
-    <ClientTaskBoard
-      completeTask={async (taskId) => {
-        'use server'
-        completeTask({ token, taskId })
-      }}
-    />
+    <>
+      <ClientSideStateUpdate assignee={assignee}>
+        <ClientTaskBoard
+          completeTask={async (taskId) => {
+            'use server'
+            completeTask({ token, taskId })
+          }}
+        />
+      </ClientSideStateUpdate>
+    </>
   )
 }
