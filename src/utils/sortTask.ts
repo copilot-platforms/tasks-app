bb9d057149ef8849f4e612fe88c44520b573abcf
aatbip <anantabipal2010@gmail.com>
2024-08-13 17:50:47 +0545

    chore:adds error handling in sortTaskByDescendingOrder

--- src/utils/sortTask.ts
+++ src/utils/sortTask.ts
@@ -1,7 +1,10 @@
 import { TaskResponse } from '@/types/dto/tasks.dto'
 
 export const sortTaskByDescendingOrder = (tasks: TaskResponse[]) => {
-  return (tasks ?? []).sort((a, b) => {
+  if (!tasks) {
+    throw new Error('Something went wrong!')
+  }
+  return tasks.sort((a, b) => {
     const dateA = new Date(a.createdAt).getTime()
     const dateB = new Date(b.createdAt).getTime()
     return dateB - dateA
