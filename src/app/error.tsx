692d96108b86ee7c27db5758669eeb38e0b3987c
Arpan Dhakal <arpandhakal20@gmail.com>
2024-08-14 17:45:00 +0545

    fix(OUT-734) added reset prop and a try again button in global error boundary

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,14 +1,17 @@
 'use client'
 
+import { PrimaryBtn } from '@/components/buttons/PrimaryBtn'
 import { isProd } from '@/config'
+import { Box, Stack } from '@mui/material'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
-const ClientErrorBoundary = ({ error }: { error: Error & { digest: string } }) => {
+const ClientErrorBoundary = ({ error, reset }: { error: Error & { digest: string }; reset: () => void }) => {
   const errorMessage =
     error.message === 'Please provide a Valid Token' ? error.message : isProd ? 'Something went wrong' : error.message
   return (
-    <div
-      style={{
+    <Box
+      sx={{
         display: 'flex',
         height: '100vh',
         marginTop: '20vh',
@@ -17,8 +20,19 @@ const ClientErrorBoundary = ({ error }: { error: Error & { digest: string } }) =
         fontFamily: 'monospace',
       }}
     >
-      {errorMessage}
-    </div>
+      <Stack direction="column" rowGap={3}>
+        {errorMessage}
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <PrimaryBtn buttonText="Try again" handleClick={() => reset()} />
+        </Box>
+      </Stack>
+    </Box>
   )
 }
 
