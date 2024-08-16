ab9389e4dec33766cbfc6dd24a31d00b78a22bb5
Rojan Rajbhandari <rrojan20@gmail.com>
2024-08-16 19:31:47 +0545

    feat(OUT-736): implement webhook notification logic for client.created

--- src/app/api/webhook/webhook.controller.ts
+++ src/app/api/webhook/webhook.controller.ts
@@ -3,6 +3,13 @@ import authenticate from '@api/core/utils/authenticate'
 import WebhookService from '@api/webhook/webhook.service'
 import { TasksService } from '@api/tasks/tasks.service'
 import { NotificationService } from '@api/notification/notification.service'
+import { HANDLEABLE_EVENT } from '@/types/webhook'
+import Bottleneck from 'bottleneck'
+import { CopilotAPI } from '@/utils/CopilotAPI'
+import { getInProductNotificationDetails } from '../notification/notification.helpers'
+import { NotificationTaskActions } from '../core/types/tasks'
+import APIError from '../core/exceptions/api'
+import httpStatus from 'http-status'
 
 export const handleWebhookEvent = async (req: NextRequest) => {
   const user = await authenticate(req)
@@ -16,8 +23,54 @@ export const handleWebhookEvent = async (req: NextRequest) => {
   }
   const { assigneeId, assigneeType } = webhookService.parseAssigneeData(webhookEvent, eventType)
 
-  // Delete corresponding tasks
   const tasksService = new TasksService(user)
+
+  if (eventType === HANDLEABLE_EVENT.ClientCreated) {
+    // First fetch all the current non-complete tasks for client's company, if exists
+    const copilot = new CopilotAPI(user.token)
+    const client = await copilot.getClient(assigneeId)
+    const company = await copilot.getCompany(client.companyId)
+    if (company.name === '') {
+      // Client does not have a fixed company!
+      return NextResponse.json({})
+    }
+
+    const tasks = await tasksService.getUnfilteredTasksForUser(company.id)
+    // Then trigger appropriate notifications
+    const bottleneck = new Bottleneck({ minTime: 250, maxConcurrent: 2 })
+    const createNotificationPromises = []
+
+    const internalUsers = (await copilot.getInternalUsers()).data
+
+    for (let task of tasks) {
+      const actionUser = internalUsers.find((iu) => iu.id === task.createdById)
+      if (!actionUser) {
+        throw new APIError(
+          httpStatus.INTERNAL_SERVER_ERROR,
+          `webhookController :: could not get action user for company task ${task.id}`,
+        )
+      }
+
+      const actionUserName = `${actionUser.givenName} ${actionUser.familyName}`
+      const inProduct = getInProductNotificationDetails(actionUserName, task, company.name)[
+        NotificationTaskActions.AssignedToCompany
+      ]
+      const notificationDetails = {
+        senderId: task.createdById,
+        recipientId: assigneeId,
+        // If any of the given action is not present in details obj, that type of notification is not sent
+        deliveryTargets: { inProduct },
+      }
+      console.log('inProduct', inProduct)
+      console.log('recip', assigneeId)
+      createNotificationPromises.push(bottleneck.schedule(() => copilot.createNotification(notificationDetails)))
+    }
+
+    await Promise.all(createNotificationPromises)
+    return NextResponse.json({ message: 'client.created webhook handled successfully' })
+  }
+
+  // Delete corresponding tasks
   console.info(`Deleting all tasks for ${assigneeType} ${assigneeId}`)
   await tasksService.deleteAllAssigneeTasks(assigneeId, assigneeType)
 
