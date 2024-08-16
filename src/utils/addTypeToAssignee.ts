92951746f6b6fab69e8071429b0961894b22f892
aatbip <anantabipal2010@gmail.com>
2024-08-16 10:33:45 +0545

    test: 6

--- src/utils/addTypeToAssignee.ts
+++ src/utils/addTypeToAssignee.ts
@@ -1,6 +1,7 @@
 import { IAssignee, IAssigneeCombined } from '@/types/interfaces'
 
-export function addTypeToAssignee(assignee: IAssignee): IAssigneeCombined[] {
+export function addTypeToAssignee(assignee?: IAssignee): IAssigneeCombined[] {
+  if (!assignee) return []
   return (Object.keys(assignee) as (keyof IAssignee)[]).flatMap((key) => {
     return assignee[key].map((assignee) => ({
       ...assignee,
