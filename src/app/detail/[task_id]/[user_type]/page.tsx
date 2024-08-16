68e0501d3f6f5754e51c25cdebdaf14e7c051cf2
Rojan Rajbhandari <rrojan20@gmail.com>
2024-08-16 17:13:56 +0545

    tryfix: prefetch causing issues?

--- src/app/detail/[task_id]/[user_type]/page.tsx
+++ src/app/detail/[task_id]/[user_type]/page.tsx
@@ -83,7 +83,10 @@ export default async function TaskDetailPage({
               <AppMargin size={SizeofAppMargin.LARGE} py="16px">
                 <Stack direction="row" justifyContent="space-between">
                   <Stack direction="row" alignItems="center" columnGap={3}>
-                    <Link href={params.user_type === UserType.INTERNAL_USER ? `/?token=${token}` : `/client?token=${token}`}>
+                    <Link
+                      href={params.user_type === UserType.INTERNAL_USER ? `/?token=${token}` : `/client?token=${token}`}
+                      prefetch={false}
+                    >
                       <SecondaryBtn
                         buttonContent={
                           <StyledTypography variant="sm" lineHeight={'21px'}>
