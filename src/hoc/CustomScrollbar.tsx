deb61b05c560e316c19f8eeccb20c4871341050c
Arpan Dhakal <arpandhakal20@gmail.com>
2024-08-18 08:56:37 +0545

    fix(OUT-678) : Unnecessary scrollbar is shown on task details page when opening from notifications
    - applied a component mounted check for customScrollbar component so that default styles would not override if the component is not mounted

--- src/hoc/CustomScrollbar.tsx
+++ src/hoc/CustomScrollbar.tsx
@@ -1,26 +1,36 @@
 'use client'
 
-import { CSSProperties, ReactNode } from 'react'
+import { CSSProperties, ReactNode, useEffect, useState } from 'react'
 import Scrollbars from 'react-custom-scrollbars'
 
 export const CustomScrollbar = ({ children, style }: { children: ReactNode; style: CSSProperties }) => {
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) return null //checking if the component mounts before applying the styles so that this component doesnt get replaced by default scrollbar if the styles are not loaded in time.
+
   return (
     <Scrollbars
       autoHide
-      renderThumbVertical={(props) => (
-        <div
-          {...props}
-          className="thumb"
-          style={{
-            borderRadius: '6px',
-            backgroundColor: 'rgba(0, 0, 0, 0.5)',
-            ...style,
-          }}
-        />
-      )}
+      renderThumbVertical={(props) => {
+        return (
+          <div
+            {...props}
+            className="thumb"
+            style={{
+              borderRadius: '6px',
+              backgroundColor: 'rgba(0, 0, 0, 0.5)',
+              ...style,
+            }}
+          />
+        )
+      }}
       autoHideTimeout={500}
       autoHideDuration={500}
-      hideTracksWhenNotNeeded={true}
+      // hideTracksWhenNotNeeded={true}
     >
       {children}
     </Scrollbars>
