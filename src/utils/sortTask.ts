b73db24a4c70d2df7388c5ca722e25f8efe11f59
aatbip <anantabipal2010@gmail.com>
2024-08-20 13:49:23 +0545

    test: 6

--- src/utils/sortTask.ts
+++ src/utils/sortTask.ts
@@ -2,9 +2,8 @@ import { TaskResponse } from '@/types/dto/tasks.dto'
 
 export const sortTaskByDescendingOrder = (tasks: TaskResponse[]) => {
   return (tasks ?? []).sort((a, b) => {
-    const dateA = new Date(a.createdAt)
-    const dateB = new Date(b.createdAt)
-    //@ts-ignore
+    const dateA = new Date(a.createdAt).getTime()
+    const dateB = new Date(b.createdAt).getTime()
     return dateB - dateA
   })
 }
