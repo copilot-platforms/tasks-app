324cc09ce39d5f2f03892cb1a4088044e02ce94b
Rojan Rajbhandari <rrojan20@gmail.com>
2024-08-29 19:45:02 +0545

    feat(OUT-537): add plus icon on each task column

--- src/components/cards/TaskColumn.tsx
+++ src/components/cards/TaskColumn.tsx
@@ -1,7 +1,10 @@
 'use client'
 
-import { Stack, Typography, styled } from '@mui/material'
+import { GrayAddIcon } from '@/icons'
+import { Box, Stack, Typography, styled } from '@mui/material'
 import { ReactNode } from 'react'
+import store from '@/redux/store'
+import { setActiveWorkflowStateId, setShowModal } from '@/redux/features/createTaskSlice'
 
 const TaskColumnHeader = styled(Stack)({
   flexDirection: 'row',
@@ -18,21 +21,33 @@ const TaskColumnContainer = styled(Stack)({
 })
 
 interface Prop {
+  workflowStateId: string
   children: ReactNode
   columnName: string
   taskCount: string
 }
 
-export const TaskColumn = ({ children, columnName, taskCount }: Prop) => {
+export const TaskColumn = ({ workflowStateId, children, columnName, taskCount }: Prop) => {
+  const handleAddBtnClicked = () => {
+    store.dispatch(setActiveWorkflowStateId(workflowStateId))
+    store.dispatch(setShowModal())
+  }
   return (
     <>
       <TaskColumnHeader>
-        <Stack direction="row" alignItems="center" columnGap={2}>
-          <Typography variant="md">{columnName}</Typography>
-          <Typography variant="sm" sx={{ color: (theme) => theme.color.gray[400], fontSize: '12px' }}>
-            {taskCount}
-          </Typography>
-        </Stack>
+        <Box sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
+          <Stack direction="row" alignItems="center" columnGap={2}>
+            <Typography variant="md">{columnName}</Typography>
+            <Typography variant="sm" sx={{ color: (theme) => theme.color.gray[400], fontSize: '12px' }}>
+              {taskCount}
+            </Typography>
+          </Stack>
+
+          {/* (22 - 16) / 2 */}
+          <Box sx={{ paddingRight: '13px', ':hover': { cursor: 'pointer' } }}>
+            <GrayAddIcon onClick={handleAddBtnClicked} />
+          </Box>
+        </Box>
       </TaskColumnHeader>
       <TaskColumnContainer>{children}</TaskColumnContainer>
     </>
