9cf29dec008178afe7052d1d0cea1c00dd8c96b5
aatbip <anantabipal2010@gmail.com>
2024-08-30 17:43:28 +0545

    feat: adds a custom drag layer for ListViewTaskCard draggable

---
+++ src/components/cards/ClientCardDragLayer.tsx
@@ -0,0 +1,88 @@
+import { Box, Stack, Typography } from '@mui/material'
+import { ViewMode } from '@prisma/client'
+import { selectTaskBoard } from '@/redux/features/taskBoardSlice'
+import { useSelector } from 'react-redux'
+import { XYCoord } from 'react-dnd'
+import { FC } from 'react'
+
+const getItemStyles = (currentOffset: XYCoord | null, previewWidth: number) => {
+  if (!currentOffset) {
+    return {
+      display: 'none',
+    }
+  }
+
+  const { x, y } = currentOffset
+  // Adjust the preview position to center it under the cursor
+  const adjustedX = x - (previewWidth - 20) // Adjust based on desired offset from the right corner
+  const adjustedY = y - 20 // Adjust the Y offset if needed
+
+  const transform = `translate(${adjustedX}px, ${adjustedY}px)`
+
+  return {
+    transform,
+    WebkitTransform: transform,
+  }
+}
+
+interface Prop {
+  currentOffset?: XYCoord | null
+  item?: { taskId: string }
+}
+
+export const ClientCardDragLayer: FC<Prop> = (props) => {
+  const { item, currentOffset } = props
+
+  const { view, tasks } = useSelector(selectTaskBoard)
+
+  const currentTask = tasks.find((el) => el.id === item?.taskId)
+
+  const previewWidth = 240 // Width of the CustomDragPreview
+
+  if (view === ViewMode.board || !currentTask || !item || !currentOffset) {
+    return null
+  }
+
+  console.log('sdjflksdf', item, currentOffset)
+
+  return (
+    <Stack
+      direction="row"
+      alignItems="center"
+      columnGap={'16px'}
+      sx={{
+        bgcolor: '#fff',
+        border: '1px solid #EFF1F4',
+        padding: '12px 20px',
+        width: `${previewWidth}px`,
+        transition: 'transform 0.1s ease',
+        ...getItemStyles(currentOffset, previewWidth),
+      }}
+    >
+      <Typography
+        variant="sm"
+        fontWeight={400}
+        sx={{
+          color: (theme) => theme.color.gray[500],
+          flexGrow: 0,
+          minWidth: '75px',
+          lineHeight: '21px',
+        }}
+      >
+        {currentTask?.label}
+      </Typography>
+      <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
+        <Typography
+          variant="sm"
+          sx={{
+            lineHeight: '21px',
+            wordBreak: 'break-word',
+            flexGrow: 1,
+          }}
+        >
+          {currentTask?.title}
+        </Typography>
+      </Box>
+    </Stack>
+  )
+}
