1ccca08fcadd73f84983c495e370a51baecc50b4
Rojan Rajbhandari <rrojan20@gmail.com>
2024-09-17 14:48:34 +0545

    fix(OUT-819): reduce the number of queries drastically + proper error handling

--- src/app/api/workers/scrap-images/scrap-images.service.ts
+++ src/app/api/workers/scrap-images/scrap-images.service.ts
@@ -19,23 +19,41 @@ export class ScrapImageService {
           lt: threeMinutesAgo, //apply oneWeekAgo. three minutes ago is used for testing
         },
       },
+      // Putting a buffer of 0.5s for each deletion (which should be more than very enough), we take 600 records at a time
+      // for each worker
+      take: 600,
     })
     const supabase = new SupabaseService()
 
-    for (const image of scrapImages) {
-      const task = await db.task.findUnique({
-        where: {
-          id: image.taskId,
-        },
-      })
-      await db.scrapImages.delete({
-        where: { id: image.id },
-      })
-      if (task && (task.body as string).includes(image.filePath)) {
-        continue
-      }
+    const tasks = await db.task.findMany({
+      where: { id: { in: scrapImages.map((image) => image.id) } },
+    })
+    const scrapImagesToDelete = []
 
-      await supabase.supabase.storage.from(supabaseBucket).remove([image.filePath])
+    for (const image of scrapImages) {
+      try {
+        // For each scrap image, check if the task still has the img url in its body
+        const task = tasks.find((_task) => _task.id === image.taskId)
+        if (!task) {
+          console.error('Could not find task for scrap image', image)
+          continue
+        }
+        // If image is in task body
+        if ((task.body || '').includes(image.filePath)) {
+          scrapImagesToDelete.push(image.id)
+          continue
+        }
+        // If image is not in task body
+        const { error } = await supabase.supabase.storage.from(supabaseBucket).remove([image.filePath])
+        if (error) {
+          console.error(error)
+          continue
+        }
+        scrapImagesToDelete.push(image.id)
+      } catch (e: unknown) {
+        console.error('What just happened...', e)
+      }
     }
+    await db.scrapImages.deleteMany({ where: { id: { in: scrapImagesToDelete } } })
   }
 }
