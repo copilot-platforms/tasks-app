93ce17099d4f1bf3934a233dfa494f8bd5ae5623
priosshrsth <shrsthprios@hotmail.com>
2024-09-19 06:35:04 +0000

    provide custom dropEffect

--- src/hoc/DragDropHandler.tsx
+++ src/hoc/DragDropHandler.tsx
@@ -3,7 +3,6 @@
 import { selectTaskBoard } from '@/redux/features/taskBoardSlice'
 import { TaskResponse } from '@/types/dto/tasks.dto'
 import { View } from '@/types/interfaces'
-import { ViewMode } from '@prisma/client'
 import { ReactNode, useEffect, useRef } from 'react'
 import { useDrag, useDrop, DropTargetMonitor } from 'react-dnd'
 import { useSelector } from 'react-redux'
@@ -23,10 +22,8 @@ interface Prop {
 export const DragDropHandler = ({
   children,
   accept,
-  index,
   id,
   task,
-  moveCard,
   onDropItem,
   draggable = false,
   droppable = false, // New prop for droppable
@@ -34,16 +31,16 @@ export const DragDropHandler = ({
   const { view } = useSelector(selectTaskBoard)
   const ref = useRef<HTMLDivElement | null>(null)
 
-  const [{ isOver, canDrop }, drop] = useDrop<{ task: TaskResponse }, unknown, { isOver: boolean; canDrop: boolean }>({
+  const [{ isOver, canDrop }, drop] = useDrop<{ id: string }, unknown, { isOver: boolean; canDrop: boolean }>({
     accept: accept,
     collect: (monitor: DropTargetMonitor) => ({
       isOver: monitor.isOver(),
       canDrop: monitor.canDrop(),
     }),
-    drop: (item, monitor) => {
+    drop: (item) => {
       if (onDropItem && id) {
         onDropItem({
-          taskId: item.task.id,
+          taskId: item.id,
           targetWorkflowStateId: id,
         })
       }
@@ -51,28 +48,23 @@ export const DragDropHandler = ({
     canDrop: () => droppable, // Only allow dropping if the component is droppable
   })
 
-  const [{ isDragging }, drag, preview] = useDrag({
-    type: accept,
-    item: () => {
-      return { task: task }
+  const [{ opacity }, drag, preview] = useDrag(
+    {
+      type: accept,
+      item: {
+        id: task?.id,
+      },
+      collect: (monitor) => ({
+        opacity: monitor.isDragging() ? 0.5 : 1,
+      }),
+      options: {
+        dropEffect: 'move',
+      },
+      canDrag: () => draggable, // Only allow dragging if the component is draggable
     },
-    collect: (monitor) => ({
-      isDragging: monitor.isDragging(),
-    }),
-    canDrag: () => draggable, // Only allow dragging if the component is draggable
-  })
+    [task?.id],
+  )
 
-  // Set an empty image as the drag preview so that the default preview is not shown
-  useEffect(() => {
-    if (window) {
-      //we only need custom drag preview in the list view
-      if (draggable) {
-        preview(new Image()) // This sets an empty drag preview
-      }
-    }
-  }, [preview, draggable])
-
-  const opacity = isDragging ? 0.5 : 1
   const dropHoverStyles =
     isOver && canDrop
       ? {
@@ -81,6 +73,16 @@ export const DragDropHandler = ({
           backgroundColor: '#F8F9FB',
         }
       : { border: '0.5px solid transparent' }
+
+  // Set an empty image as the drag preview so that the default preview is not shown
+  useEffect(() => {
+    if (window) {
+      //we only need custom drag preview in the list view
+      if (draggable) {
+        //  preview(new Image()) // This sets an empty drag preview
+      }
+    }
+  }, [preview, draggable])
 
   if (draggable) {
     drag(drop(ref)) // If draggable, combine drag and drop refs
