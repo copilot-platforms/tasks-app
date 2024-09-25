b08e3ecc9325d2bbff15500b7a24011a9f09578b
Rojan Rajbhandari <rrojan20@gmail.com>
2024-09-25 20:37:02 +0545

    feat(OUT-837): add retry util for CopilotAPI

--- src/app/api/core/utils/withRetry.ts
+++ src/app/api/core/utils/withRetry.ts
@@ -6,12 +6,14 @@ export const withRetry = <T>(fn: (...args: any[]) => Promise<T>, args: any[]): P
     minTimeout: 500,
     maxTimeout: 500,
     factor: 1, // No exponential delays for now
+
     onFailedAttempt: (error: FailedAttemptError) => {
       console.error(
         `CopilotAPI#withRetry - Attempt ${error.attemptNumber} failed. There are ${error.retriesLeft} retries left. Error:`,
         error,
       )
     },
+
     shouldRetry: (error: any) => {
       // Typecasting because Copilot doesn't export an error class
       const err = error as { status?: number }
