410166676021afda2f8e08555d2fbd84b5ce2e4a
Rojan Rajbhandari <rrojan20@gmail.com>
2024-09-25 20:36:04 +0545

    chore(OUT-837): add p-retry package

---
+++ src/app/api/core/utils/withRetry.ts
@@ -0,0 +1,22 @@
+import pRetry, { FailedAttemptError } from 'p-retry'
+
+export const withRetry = <T>(fn: (...args: any[]) => Promise<T>, args: any[]): Promise<T> => {
+  return pRetry(() => fn(...args), {
+    retries: 3,
+    minTimeout: 500,
+    maxTimeout: 500,
+    factor: 1, // No exponential delays for now
+    onFailedAttempt: (error: FailedAttemptError) => {
+      console.error(
+        `CopilotAPI#withRetry - Attempt ${error.attemptNumber} failed. There are ${error.retriesLeft} retries left. Error:`,
+        error,
+      )
+    },
+    shouldRetry: (error: any) => {
+      // Typecasting because Copilot doesn't export an error class
+      const err = error as { status?: number }
+      // Retry only if statusCode === 429
+      return err.status === 429
+    },
+  })
+}
