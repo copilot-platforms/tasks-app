9311bcec8a782562af5fe07422230f206d07f690
Rojan Rajbhandari <59971845+rrojan@users.noreply.github.com>
2024-09-27 10:33:40 +0545

    OUT-837 | OUT-836 | Add retry & delay mechanism to CopilotAPI (#312)
    * fix(OUT-837): proper error handling for Copilot API responses
    
    * chore(OUT-837): add p-retry package
    
    * feat(OUT-837): add retry util for CopilotAPI
    
    * refactor(OUT-837): clean code
    
    * refactor(OUT-837): stupid typo
    
    * feat(OUT-837): implement retry in copilot auth endpoint
    
    * refactor(OUT-837): organize imports
    
    * perf(OUT-837): add exponential factor of 2 to retry delay
    
    * feat(OUT-837): retries & delays are now a thing of beauty
    
    * feat(OUT-837): hopefully sentry doesn't report retry errors now
    
    * fix(OUT-837): remove unnecessary console.ewarn
    
    * chore(OUT-837): avoid adding duplicate event processors to Sentry
    
    * refactor(OUT-837): clean code
    
    * refactor(OUT-837): clean code

--- src/types/CopilotApiError.ts
+++ src/types/CopilotApiError.ts
@@ -1,3 +1,14 @@
+// Interfaces that implement a particular behavior of CopilotApiError
+export interface StatusableError extends Error {
+  status: number
+}
+
+export interface MessagableError extends Error {
+  body?: {
+    message: string
+  }
+}
+
 export class CopilotApiError extends Error {
   readonly status: number
   readonly body: {
