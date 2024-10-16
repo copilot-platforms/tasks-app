70dd6b89ac6f9383bc2425cc48ccf16ddebb0e68
aatbip <anantabipal2010@gmail.com>
2024-10-16 16:57:27 +0545

    chore: adds error logging at fetcher

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,5 +1,7 @@
 export const fetcher = (url: string | null) => {
   if (url) {
-    return fetch(url).then((res) => res.json())
+    return fetch(url)
+      .then((res) => res.json())
+      .catch((e) => console.log('fetcher: ', e))
   }
 }
