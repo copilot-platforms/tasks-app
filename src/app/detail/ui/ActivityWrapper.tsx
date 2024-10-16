c195727e0e063386ce8ab7ba675b93a66bb0367d
aatbip <anantabipal2010@gmail.com>
2024-10-16 16:53:42 +0545

    chore: removes activity log reducers

--- src/app/detail/ui/ActivityWrapper.tsx
+++ src/app/detail/ui/ActivityWrapper.tsx
@@ -15,7 +15,6 @@ export const ActivityWrapper = ({ token, task_id }: { token: string; task_id: st
   const { data: activities, isLoading } = useSWR(`/api/tasks/${task_id}/activity-logs/?token=${token}`, fetcher, {
     refreshInterval: 1000,
   })
-  console.log('activities', activities)
 
   return (
     <Box width="100%">
--- src/redux/features/taskDetailsSlice.ts
+++ src/redux/features/taskDetailsSlice.ts
@@ -1,20 +1,17 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { RootState } from '../store'
 import { IAssigneeSuggestions } from '@/types/interfaces'
-import { LogResponse } from '@/app/api/activity-logs/schemas/LogResponseSchema'
 
 interface IInitialState {
   showConfirmDeleteModal: boolean
   showSidebar: boolean
   assigneeSuggestions: IAssigneeSuggestions[]
-  activities: LogResponse[]
 }
 
 const initialState: IInitialState = {
   showConfirmDeleteModal: false,
   showSidebar: true,
   assigneeSuggestions: [],
-  activities: [],
 }
 
 const taskDetailsSlice = createSlice({
@@ -30,14 +27,11 @@ const taskDetailsSlice = createSlice({
     setAssigneeSuggestion: (state, action: { payload: IAssigneeSuggestions[] }) => {
       state.assigneeSuggestions = action.payload
     },
-    setActivities: (state, action: { payload: LogResponse[] }) => {
-      state.activities = action.payload
-    },
   },
 })
 
 export const selectTaskDetails = (state: RootState) => state.taskDetail
 
-export const { setShowConfirmDeleteModal, setShowSidebar, setAssigneeSuggestion, setActivities } = taskDetailsSlice.actions
+export const { setShowConfirmDeleteModal, setShowSidebar, setAssigneeSuggestion } = taskDetailsSlice.actions
 
 export default taskDetailsSlice.reducer
