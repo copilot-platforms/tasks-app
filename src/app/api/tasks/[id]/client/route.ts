caab4eb318c3375b9855f31c6142827f9c3c742c
Rojan Rajbhandari <rrojan20@gmail.com>
2024-10-23 15:37:18 +0545

    fix(OUT-950): raise vercel serverless function duration

--- src/app/api/tasks/[id]/client/route.ts
+++ src/app/api/tasks/[id]/client/route.ts
@@ -1,4 +1,6 @@
 import { withErrorHandler } from '@/app/api/core/utils/withErrorHandler'
 import { clientUpdateTask } from '@/app/api/tasks/tasks.controller'
 
+export const maxDuration = 300
+
 export const PATCH = withErrorHandler(clientUpdateTask)
