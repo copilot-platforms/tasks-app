b1f0fc4cbbdcd6cd21c45f5fd310e13e1a075533
Rojan Rajbhandari <rrojan20@gmail.com>
2024-10-24 17:51:41 +0545

    feat(OUT-956): add script to populate missing TASK_CREATED activity logs

---
+++ src/cmd/fill-activity-logs/index.ts
@@ -0,0 +1,46 @@
+import { SchemaByActivityType } from '@/app/api/activity-logs/const'
+import DBClient from '@/lib/db'
+import { ActivityLog, ActivityType, AssigneeType } from '@prisma/client'
+import { z } from 'zod'
+
+/**
+ * cmd script to fill up 'created by X on Y' activity logs for tasks
+ */
+const run = async () => {
+  const db = DBClient.getInstance()
+  // Gets all non-soft deleted tasks that don't have a corresponding activity log pointing to it
+  const tasks = await db.task.findMany({
+    where: { activityLog: { none: { type: ActivityType.TASK_CREATED } } },
+  })
+  if (!tasks.length) {
+    console.info('All clear captain!')
+  }
+  console.warn(`⚠️ ${tasks.length} tasks don't have a Task Created log yet!`)
+  const data = []
+
+  for (let i = 0; i < tasks.length; i++) {
+    const task = tasks[i]
+    console.info(`    🛠️ Inserting activity log ${i}/${tasks.length} for task ${task.id} (${task.title})...`)
+    const details = {
+      id: task.id,
+      body: '',
+      title: task.title,
+      assigneeId: task.assigneeId,
+      workspaceId: task.workspaceId,
+      assigneeType: task.assigneeType,
+    }
+    data.push({
+      taskId: task.id,
+      workspaceId: task.workspaceId,
+      type: ActivityType.TASK_CREATED,
+      userId: task.createdById,
+      userRole: AssigneeType.internalUser,
+      details,
+      createdAt: task.createdAt,
+    })
+  }
+
+  await db.activityLog.createMany({ data })
+}
+
+run()
