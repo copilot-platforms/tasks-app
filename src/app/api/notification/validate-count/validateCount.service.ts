79c606de34d1abe7b21eba3e2bdd4884d618e1f5
Rojan Rajbhandari <rrojan20@gmail.com>
2024-10-29 17:23:49 +0545

    feat(OUT-934): finish up validate-count endpoint

--- src/app/api/notification/validate-count/validateCount.service.ts
+++ src/app/api/notification/validate-count/validateCount.service.ts
@@ -1,93 +1,129 @@
-import { NotificationCreatedResponse } from '@/types/common'
-import { CopilotAPI } from '@/utils/CopilotAPI'
-import APIError from '@api/core/exceptions/api'
-import { NotificationService } from '@api/notification/notification.service'
-import httpStatus, { extra } from 'http-status'
-import { z } from 'zod'
-import { TasksService } from '../../tasks/tasks.service'
-import { UserRole } from '../../core/types/user'
+import { getArrayDifference } from '@/utils/array'
 import { bottleneck } from '@/utils/bottleneck'
+import { CopilotAPI } from '@/utils/CopilotAPI'
+import User from '@api/core/models/User.model'
+import { UserRole } from '@api/core/types/user'
+import { NotificationService } from '@api/notification/notification.service'
+import { TasksService } from '@api/tasks/tasks.service'
+import { ClientNotification, Task } from '@prisma/client'
 
 export class ValidateCountService extends NotificationService {
-  // Queries for notifications from CopilotAPI and fixes it if not in sync with task count
-  async fixClientNotificationCount(): Promise<void> {
-    const userId = z.string().safeParse(this.user.clientId)
-    if (userId.error) {
-      throw new APIError(httpStatus.UNAUTHORIZED, 'Only client users are allowed to access this feature')
-    }
-    const clientId = userId.data
+  private readonly copilot: CopilotAPI
+  constructor(user: User) {
+    super(user)
+    this.copilot = new CopilotAPI(user.token)
+  }
 
-    const copilot = new CopilotAPI(this.user.token)
-    const notifications = await copilot.getNotifications(clientId, { limit: 1_000_000 })
-
+  /**
+   * Queries for notifications from CopilotAPI and fixes it if not in sync with task count
+   * @param {string} clientId - Copilot client id for which notification fix has to be done
+   */
+  async fixClientNotificationCount(clientId: string): Promise<void> {
+    const notifications = await this.copilot.getNotifications(clientId, { limit: 1_000_000 })
     await this.validateWithTasks(
-      copilot,
+      clientId,
       notifications.map((n) => n.id),
     )
   }
 
   /**
-   * Validates task count with notification count, and creates / removes notifications as necessary
+   * Validates a list of Copilot notifications with tasks and task notifications
+   * Creates / removes notifications from Copilot as necessary
+   * @param {string[]} copilotNotificationIds - Notification Ids for a particular user for an app in Copilot
    */
-  private async validateWithTasks(copilot: CopilotAPI, copilotNotificationIds: string[]): Promise<void> {
+  private async validateWithTasks(clientId: string, copilotNotificationIds: string[]): Promise<void> {
     const tasksService = new TasksService(this.user)
     const tasks = await tasksService.getAllTasks()
-    console.log('t', tasks)
-    const recipientId = z.string().parse(this.user.clientId)
 
+    console.log('tasks', tasks.length)
+    // Query all notifications triggered for a list of client/company tasks
     const appNotifications = await this.getAllForTasks<UserRole.Client>(tasks)
     const appNotificationIds = appNotifications.map((n) => n.notificationId)
-    console.log('copilot', copilotNotificationIds)
-    console.log('app', appNotificationIds)
 
+    console.log('app', appNotificationIds)
+    console.log('copilot', copilotNotificationIds)
+
+    console.log('a', getArrayDifference(appNotificationIds, copilotNotificationIds).length)
+    if (getArrayDifference(appNotificationIds, copilotNotificationIds).length) {
+      await this.removeOrphanNotificationsFromDb(appNotificationIds, copilotNotificationIds)
+    }
+
+    if (tasks.length !== copilotNotificationIds.length) {
+      await this.addMissingNotifications(tasks, clientId, appNotifications)
+    }
+
+    // Do this after fixing everything else to avoid removing necessary notifications
+    console.log('b', getArrayDifference(copilotNotificationIds, appNotificationIds).length)
+    if (getArrayDifference(copilotNotificationIds, appNotificationIds).length) {
+      await this.removeOrphanNotificationsFromCopilot(appNotificationIds, copilotNotificationIds)
+    }
+  }
+
+  /**
+   * Removes notifications present in db, which are not present in Copilot
+   */
+  private async removeOrphanNotificationsFromDb(appNotificationIds: string[], copilotNotificationIds: string[]) {
+    console.info('Removing orphan notifications from db')
     // Delete orphan notifications (notifications in db that don't exist in Copilot)
     const orphanNotifications = appNotificationIds.filter((id) => !copilotNotificationIds.includes(id))
     if (orphanNotifications.length) {
-      console.log('or', orphanNotifications)
       await this.db.clientNotification.deleteMany({ where: { notificationId: { in: orphanNotifications } } })
     }
+  }
 
-    // --- Main issue: notifications not in copilot for existing tasks
-    if (tasks.length !== copilotNotificationIds.length) {
-      const tasksWithNotifications = appNotifications.map((n) => n.taskId)
-      const tasksWithoutNotifications = tasks.filter((task) => !tasksWithNotifications.includes(task.id))
-      const createNotificationPromises = []
-      for (const task of tasksWithoutNotifications) {
-        createNotificationPromises.push(
-          bottleneck.schedule(() => {
-            console.info(`Creating missing notification for task ${task.id} - ${task.title}`)
-            return copilot.createNotification({
-              senderId: task.createdById,
-              recipientId,
-              deliveryTargets: {
-                inProduct: {
-                  // doesn't matter what you add here since notification details cannot be viewed
-                  title: task.id,
-                },
-              },
-            })
-          }),
-        )
-      }
-      const newNotifications = await Promise.all(createNotificationPromises)
-      const newClientNotificationData = []
-      for (const i in newNotifications) {
-        newClientNotificationData.push({
-          notificationId: newNotifications[i].id,
-          taskId: tasks[i].id,
-          clientId: recipientId,
-        })
-      }
-      await this.db.clientNotification.createMany({
-        data: newClientNotificationData,
-      })
-
-      // -- Handle copilotNotifications having notifications that appNotifications does not
-      const extraCopilotNotifications = copilotNotificationIds.filter((id) => !appNotificationIds.includes(id))
-      if (extraCopilotNotifications.length) {
-        console.log("There are notification in Copilot that aren't in db. Fixing...")
-        await this.bulkMarkAsRead(extraCopilotNotifications)
-      }
+  /**
+   * Cleans up and removes notifications present in Copilot, which are not present in db
+   */
+  private async removeOrphanNotificationsFromCopilot(appNotificationIds: string[], copilotNotificationIds: string[]) {
+    console.info('Removing orphan notifications from copilot')
+    // -- Handle copilotNotifications having notifications that appNotifications does not
+    const extraCopilotNotifications = copilotNotificationIds.filter((id) => !appNotificationIds.includes(id))
+    if (extraCopilotNotifications.length) {
+      await this.bulkMarkAsRead(extraCopilotNotifications)
     }
+  }
+
+  /**
+   * Bulk-adds missing task notifications to Copilot client for tasks which are present in Tasks table
+   * but does not have an associated notification
+   */
+  private async addMissingNotifications(tasks: Task[], clientId: string, appNotifications: ClientNotification[]) {
+    // First create missing notifications in Copilot in-product
+    const tasksWithNotifications = appNotifications.map((n: any) => n.taskId)
+    console.log('app2', appNotifications)
+    const tasksWithoutNotifications = tasks.filter((task) => !tasksWithNotifications.includes(task.id))
+    console.log('without', tasksWithoutNotifications)
+    const createNotificationPromises = []
+    for (const task of tasksWithoutNotifications) {
+      createNotificationPromises.push(
+        bottleneck.schedule(() => {
+          console.info(`Creating missing notification for task ${task.id} - ${task.title}`)
+          return this.copilot.createNotification({
+            senderId: task.createdById,
+            recipientId: clientId,
+            deliveryTargets: {
+              inProduct: {
+                // doesn't matter what you add here since notification details cannot be viewed
+                title: task.id,
+              },
+            },
+          })
+        }),
+      )
+    }
+    console.log('createNotificationPromises', createNotificationPromises)
+    const newNotifications = await Promise.all(createNotificationPromises)
+    // Now track those to ClientNotifications table
+    const newClientNotificationData = []
+    for (const i in newNotifications) {
+      newClientNotificationData.push({
+        notificationId: newNotifications[i].id,
+        taskId: tasks[i].id,
+        clientId,
+      })
+    }
+    await this.db.clientNotification.createMany({
+      data: newClientNotificationData,
+    })
   }
 }
