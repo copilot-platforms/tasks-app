d79d905cf4e4ea1a26462ee47ae36258b9302ca2
Arpan Dhakal <arpandhakal20@gmail.com>
2024-11-06 13:39:27 +0545

    feat(OUT-884) : loading state added for attachments

--- src/components/AttachmentLayout.tsx
+++ src/components/AttachmentLayout.tsx
@@ -2,7 +2,7 @@
 
 import { DownloadBtn, PdfIcon } from '@/icons'
 import { attachmentIcons } from '@/utils/iconMatcher'
-import { Box, Stack, Typography } from '@mui/material'
+import { Box, Skeleton, Stack, Typography } from '@mui/material'
 import React from 'react'
 import { IconBtn } from './buttons/IconBtn'
 import { SupabaseActions } from '@/utils/SupabaseActions'
@@ -14,13 +14,16 @@ const AttachmentLayout = ({
   fileName,
   fileSize,
   fileType,
+  isUploading,
 }: {
   selected: boolean
   src: string
   fileName: string
   fileSize: string
   fileType: string
+  isUploading: boolean
 }) => {
+  console.log(isUploading)
   const supabaseActions = new SupabaseActions()
   const handleDownload = () => {
     const filePath = getFilePathFromUrl(src)
@@ -28,10 +31,49 @@ const AttachmentLayout = ({
       supabaseActions.downloadAttachment(filePath, fileName)
     }
   }
+
+  if (isUploading) {
+    return (
+      <Box
+        sx={{
+          padding: '4px 8px',
+          marginTop: '8px',
+          marginBottom: '8px',
+          maxWidth: '100%',
+          border: (theme) => (selected ? `1px solid ${theme.color.gray[600]}` : `1px solid ${theme.color.gray[150]}`),
+          borderRadius: '4px',
+          background: '#fff',
+          boxShadow: '0px 0px 24px 0px rgba(0, 0, 0, 0.07)',
+        }}
+      >
+        <Stack justifyContent={'space-between'} direction="row" alignItems="center">
+          <Stack direction="row" columnGap="5.5px" alignItems="center">
+            <Skeleton variant="rectangular" width={24} height={24} />
+            <Stack direction="column">
+              <Skeleton variant="text" width={120} height={20} />
+              <Skeleton variant="text" width={80} height={16} />
+            </Stack>
+          </Stack>
+          <Box
+            className="download-btn"
+            sx={{
+              opacity: 0,
+              transition: 'opacity 0.2s ease',
+            }}
+          >
+            {/* Skeleton for the download button */}
+            <Skeleton variant="circular" width={24} height={24} />
+          </Box>
+        </Stack>
+      </Box>
+    )
+  }
   return (
     <Box
       sx={{
         padding: '4px 8px',
+        marginTop: '8px',
+        marginBottom: '8px',
         maxWidth: '100%',
         border: (theme) => (selected ? `1px solid ${theme.color.gray[600]}` : `1px solid ${theme.color.gray[150]}`),
         borderRadius: '4px',
