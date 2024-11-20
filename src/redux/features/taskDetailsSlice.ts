dbc628cd52161ec1da0a5c0eb0923df3bac54454
Ananta Bipal <anantabipal2010@gmail.com>
2024-11-20 14:23:02 +0545

    OUT 1017, OUT-1018 API Integration for Archive / Unarchive button (#416)
    * feat:adds button components for archiving
    
    * chore(OUT-1016): add migrations & model changes for isArchived and lastArchivedDate
    
    * chore(OUT-1016): add isArchived to update DTO
    
    * feat(OUT-1016): add isArchived support for IU and CU
    
    * fix(OUT-1016): type issue with load tester seed method
    
    * feat:archive api integrations
    
    * feat:archive api integrations
    
    * chore:remove logs
    
    * chore:relative imports
    
    * fix:removes revalidate true
    
    * test: 1
    
    * test: 2
    
    * test: 3
    
    * test: 4
    
    * test: 6
    
    * fix:adds validation for Z in getTimeDifference
    
    * test: 7
    
    ---------
    
    Co-authored-by: Rojan Rajbhandari <rrojan20@gmail.com>
    Co-authored-by: Rojan Rajbhandari <59971845+rrojan@users.noreply.github.com>

--- src/redux/features/taskDetailsSlice.ts
+++ src/redux/features/taskDetailsSlice.ts
@@ -1,17 +1,20 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { RootState } from '../store'
 import { IAssigneeSuggestions } from '@/types/interfaces'
+import { TaskResponse } from '@/types/dto/tasks.dto'
 
 interface IInitialState {
   showConfirmDeleteModal: boolean
   showSidebar: boolean
   assigneeSuggestions: IAssigneeSuggestions[]
+  task: TaskResponse | undefined
 }
 
 const initialState: IInitialState = {
   showConfirmDeleteModal: false,
   showSidebar: true,
   assigneeSuggestions: [],
+  task: undefined,
 }
 
 const taskDetailsSlice = createSlice({
@@ -27,11 +30,14 @@ const taskDetailsSlice = createSlice({
     setAssigneeSuggestion: (state, action: { payload: IAssigneeSuggestions[] }) => {
       state.assigneeSuggestions = action.payload
     },
+    setTask: (state, action: { payload: TaskResponse }) => {
+      state.task = action.payload
+    },
   },
 })
 
 export const selectTaskDetails = (state: RootState) => state.taskDetail
 
-export const { setShowConfirmDeleteModal, setShowSidebar, setAssigneeSuggestion } = taskDetailsSlice.actions
+export const { setShowConfirmDeleteModal, setShowSidebar, setAssigneeSuggestion, setTask } = taskDetailsSlice.actions
 
 export default taskDetailsSlice.reducer
