07977a3174ca37e8dab77c09a02eb5ed9c82a224
aatbip <anantabipal2010@gmail.com>
2024-11-27 15:13:09 +0545

    test:2

--- src/utils/sortTask.ts
+++ src/utils/sortTask.ts
@@ -1,9 +1,17 @@
 import { TaskResponse } from '@/types/dto/tasks.dto'
 
 export const sortTaskByDescendingOrder = (tasks: TaskResponse[]) => {
-  return [...tasks].sort((a, b) => {
-    const dateA = new Date(a.createdAt).getTime()
-    const dateB = new Date(b.createdAt).getTime()
-    return dateB - dateA
-  })
+  return [...tasks]
+    .map((task) => {
+      const createdAt = typeof task.createdAt === 'string' ? task.createdAt : new Date(task.createdAt).toISOString() // Convert Date to ISO string
+      return {
+        ...task,
+        createdAt: createdAt.endsWith('Z') ? createdAt : `${createdAt}Z`, // Ensure trailing 'Z'
+      }
+    })
+    .sort((a, b) => {
+      const dateA = new Date(a.createdAt).getTime()
+      const dateB = new Date(b.createdAt).getTime()
+      return dateB - dateA
+    })
 }
