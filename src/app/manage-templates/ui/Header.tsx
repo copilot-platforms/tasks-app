913d2c9429a02caa2c4d4f2f2a719f88a976ac9e
Arpan Dhakal <arpandhakal20@gmail.com>
2024-12-10 16:30:49 +0545

    fix(OUT-1123) : created a custom component for breadcrumbs and header which is now resuable for Manage templates and Task details page

--- src/app/manage-templates/ui/Header.tsx
+++ src/app/manage-templates/ui/Header.tsx
@@ -8,31 +8,19 @@ import { StyledBox, StyledKeyboardIcon, StyledTypography } from '@/app/detail/ui
 import store from '@/redux/store'
 import { setShowTemplateModal } from '@/redux/features/templateSlice'
 import { TargetMethod } from '@/types/interfaces'
-import { useRouter } from 'next/navigation'
 import { useSelector } from 'react-redux'
 import { selectTaskBoard } from '@/redux/features/taskBoardSlice'
 import { IconBtn } from '@/components/buttons/IconBtn'
 import { Add } from '@mui/icons-material'
+import { HeaderBreadcrumbs } from '@/components/layouts/HeaderBreadcrumbs'
 
 export const ManageTemplateHeader = ({ showNewTemplateButton }: { showNewTemplateButton: boolean }) => {
-  const router = useRouter()
   const { token } = useSelector(selectTaskBoard)
   return (
     <StyledBox>
       <AppMargin size={SizeofAppMargin.LARGE} py="16px">
         <Stack direction="row" justifyContent="space-between" alignItems="center">
-          <Stack direction="row" alignItems="center" columnGap={3}>
-            <SecondaryBtn
-              buttonContent={
-                <StyledTypography variant="sm" onClick={() => router.push(`/?token=${token}`)}>
-                  Tasks
-                </StyledTypography>
-              }
-              enableBackground
-            />
-            <StyledKeyboardIcon />
-            <Typography variant="sm">Manage Templates</Typography>
-          </Stack>
+          <HeaderBreadcrumbs title="Manage templates" token={token} />
           {showNewTemplateButton && (
             <>
               <Box
