ac7c789dd950197d267924745c4bcd55045fa28b
Arpan Dhakal <arpandhakal20@gmail.com>
2024-12-18 13:52:16 +0545

    fix(OUT-1156) : selector component design issues

--- src/components/inputs/Autocomplete.tsx
+++ src/components/inputs/Autocomplete.tsx
@@ -1,18 +1,29 @@
 import { Autocomplete, styled } from '@mui/material'
 
-export const StyledAutocomplete = styled(Autocomplete)({
+export const StyledAutocomplete = styled(Autocomplete)(({ theme }) => ({
+  '& .MuiOutlinedInput-root': {
+    borderRadius: '4px',
+    padding: '6px 14px 10px 14px',
+    paddingRight: '14px !important',
+  },
+
+  '& .MuiOutlinedInput-root.Mui-focused': {
+    borderRadius: '4px 4px 0px 0px',
+  },
+  '& fieldset.MuiOutlinedInput-notchedOutline': {
+    borderWidth: '1px',
+  },
+  '& .MuiAutocomplete-inputRoot .MuiAutocomplete-input': {
+    padding: '0px',
+  },
+  '& .MuiAutocomplete-popper div': {
+    borderTopRightRadius: '0px',
+    borderTopLeftRadius: '0px',
+  },
   '& .MuiAutocomplete-endAdornment': {
     display: 'none',
-  },
-  '& .MuiOutlinedInput-root': {
-    padding: 0,
-    border: '1px solid #EDEDF0',
   },
   '&:hover .MuiOutlinedInput-notchedOutline': {
     border: '1px solid #EDEDF0',
   },
-  '& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline': {
-    border: '1px solid #EDEDF0',
-    padding: 0,
-  },
-})
+}))
