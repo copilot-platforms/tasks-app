3a6eff0f71e7163801882218ddd0a657054b2b13
Rojan Rajbhandari <rrojan20@gmail.com>
2024-12-18 17:20:42 +0545

    fix(OUT-1163): fix User#role delegation logic

--- src/utils/previewMode.ts
+++ src/utils/previewMode.ts
@@ -1,6 +1,8 @@
 import { setCreateTaskFields } from '@/redux/features/createTaskSlice'
+import { setFilterOptions } from '@/redux/features/taskBoardSlice'
 import store from '@/redux/store'
 import { PreviewMode, Token } from '@/types/common'
+import { FilterOptions } from '@/types/interfaces'
 
 export const handlePreviewMode = (previewMode: NonNullable<PreviewMode>, tokenPayload: Token) => {
   // If clientId is provided, ignore corresponding companyId. Else pick up the companyId
@@ -9,6 +11,5 @@ export const handlePreviewMode = (previewMode: NonNullable<PreviewMode>, tokenPa
     throw new Error('Could not find preview client / company id')
   }
 
-  store.dispatch(setCreateTaskFields({ targetField: 'assigneeId', value: previewId }))
-  store.dispatch(setCreateTaskFields({ targetField: 'assigneeType', value: previewMode }))
+  store.dispatch(setFilterOptions({ optionType: FilterOptions.ASSIGNEE, newValue: previewId }))
 }
