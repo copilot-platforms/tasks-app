023b144c8cdd5b337120723d2a99a8bd4a7b0f74
Arpan Dhakal <arpandhakal20@gmail.com>
2024-12-19 12:31:04 +0545

    fix(OUT-1156) : selector component design issue fixed

--- src/components/inputs/Autocomplete.tsx
+++ src/components/inputs/Autocomplete.tsx
@@ -1,12 +1,26 @@
+import React from 'react'
 import { Autocomplete, styled } from '@mui/material'
 
-export const StyledAutocomplete = styled(Autocomplete)({
+interface StyledAutocompleteProps {
+  placement?: string
+}
+
+export const StyledAutocomplete = styled(Autocomplete, {
+  shouldForwardProp: (prop) => prop !== 'placement',
+})<StyledAutocompleteProps>(({ theme, placement }) => ({
   '& .MuiAutocomplete-endAdornment': {
     display: 'none',
   },
   '& .MuiOutlinedInput-root': {
-    padding: 0,
-    border: '1px solid #EDEDF0',
+    padding: placement === 'top' ? '8px 12px 4px 12px' : '4px 12px 8px 12px',
+    paddingRight: '12px !important',
+    '& .MuiAutocomplete-input': {
+      padding: '0px',
+      height: '21px',
+    },
+  },
+  '& .MuiOutlinedInput-root.Mui-focused': {
+    borderRadius: placement === 'top' ? '0px 0px 4px 4px' : '4px 4px 0px 0px',
   },
   '&:hover .MuiOutlinedInput-notchedOutline': {
     border: '1px solid #EDEDF0',
@@ -15,4 +29,4 @@ export const StyledAutocomplete = styled(Autocomplete)({
     border: '1px solid #EDEDF0',
     padding: 0,
   },
-})
+}))
--- src/components/inputs/ListComponent.tsx
+++ src/components/inputs/ListComponent.tsx
@@ -12,7 +12,7 @@ export interface ListComponentProps extends Omit<ScrollbarProps, 'ref'> {
 
 const ListComponentInternal = forwardRef<Scrollbars, ListComponentProps>((props, ref) => {
   const { children, endOption, endOptionHref, autoHeightMax, ...comProps } = props
-  const xs = useMediaQuery('(max-width:600px) or (max-height:800px)')
+  const xs = useMediaQuery('(max-width:600px)')
   const router = useRouter()
 
   return (
@@ -36,7 +36,7 @@ const ListComponentInternal = forwardRef<Scrollbars, ListComponentProps>((props,
               ...viewProps.style,
               borderRadius: '0',
               position: 'relative',
-              maxHeight: autoHeightMax ?? (xs ? '17vh' : '30vh'),
+              maxHeight: autoHeightMax ?? (xs ? '175px' : '291px'),
               inset: '0px',
               overflow: 'auto',
               paddingBottom: '7px',
@@ -45,7 +45,7 @@ const ListComponentInternal = forwardRef<Scrollbars, ListComponentProps>((props,
         )}
         autoHide
         autoHeight
-        autoHeightMax={autoHeightMax ?? (xs ? '15vh' : '30vh')}
+        autoHeightMax={autoHeightMax ?? (xs ? '172px' : '291px')}
         autoHideTimeout={500}
         autoHideDuration={500}
         hideTracksWhenNotNeeded
@@ -59,5 +59,9 @@ const ListComponentInternal = forwardRef<Scrollbars, ListComponentProps>((props,
 ListComponentInternal.displayName = 'ListComponentInternal'
 
 export const ListComponent = (
-  props: JSX.IntrinsicElements['div'] & { endOption?: React.ReactNode; endOptionHref?: string; autoHeightMax?: string },
+  props: JSX.IntrinsicElements['div'] & {
+    endOption?: React.ReactNode
+    endOptionHref?: string
+    autoHeightMax?: string
+  },
 ) => <ListComponentInternal {...(props as ListComponentProps)} />
