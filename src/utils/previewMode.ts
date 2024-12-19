a9cab4250c03713e5a092ab0fe3a6f865c9b8431
Rojan Rajbhandari <rrojan20@gmail.com>
2024-12-19 19:33:38 +0545

    feat(OUT-1163): add support for previewMode assignee fetching

--- src/utils/previewMode.ts
+++ src/utils/previewMode.ts
@@ -1,8 +1,14 @@
-import { setCreateTaskFields } from '@/redux/features/createTaskSlice'
 import { setFilterOptions } from '@/redux/features/taskBoardSlice'
 import store from '@/redux/store'
 import { PreviewMode, Token } from '@/types/common'
 import { FilterOptions } from '@/types/interfaces'
+
+export const getPreviewMode = (tokenPayload: Token): PreviewMode => {
+  const isClientPreview = tokenPayload.internalUserId && tokenPayload.clientId
+  const isCompanyPreview = tokenPayload.internalUserId && tokenPayload.companyId === 'default'
+  const previewMode: PreviewMode = isClientPreview ? 'client' : isCompanyPreview ? 'company' : null
+  return previewMode
+}
 
 export const handlePreviewMode = (previewMode: NonNullable<PreviewMode>, tokenPayload: Token) => {
   // If clientId is provided, ignore corresponding companyId. Else pick up the companyId
