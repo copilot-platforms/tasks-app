6b64d3cf683c6baa135ce8fb87e394d0c109ca6f
Arpan Dhakal <arpandhakal20@gmail.com>
2024-12-23 17:49:01 +0545

    fix(OUT-1176) : switching between view shows blank screen, added a check for tasks rather than filteredtask on board view

--- src/app/ui/TaskBoard.tsx
+++ src/app/ui/TaskBoard.tsx
@@ -119,7 +119,7 @@ export const TaskBoard = ({ mode }: TaskBoardProps) => {
           await updateViewModeSettings(z.string().parse(token), payload)
         }}
       />
-      {isNoTasksWithFilter && <NoFilteredTasksState />}
+      {tasks && tasks.length === 0 && <NoFilteredTasksState />}
 
       {!isNoTasksWithFilter && viewBoardSettings === View.BOARD_VIEW && (
         <Box sx={{ padding: '12px 12px' }}>
