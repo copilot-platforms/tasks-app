4e22488c686af5cb49b36ba7b0c74e0cc428730e
Rojan Rajbhandari <rrojan20@gmail.com>
2025-01-08 13:42:53 +0545

    revert(OUT-1238): changes from another PR

--- src/cmd/fill-activity-logs/index.ts
+++ src/cmd/fill-activity-logs/index.ts
@@ -1,11 +1,11 @@
-import { fillTaskCreated } from './fillTaskCreated'
+// import { fillTaskCreated } from './fillTaskCreated'
 // Use to fill "X created task Y" records for tasks without them
-fillTaskCreated()
+// fillTaskCreated()
 
 // import { fillTaskAssigned } from './fillTaskAssigned'
 // Use to fill "X assigned task to Y" activity logs for tasks without them
 // fillTaskAssigned()
 
-// import { fillWorkflowStateUpdated } from './fillWorkflowStateUpdated'
+import { fillWorkflowStateUpdated } from './fillWorkflowStateUpdated'
 // Use to fill "X changed status from Y to Z" activity logs for tasks with old corrupted details
-// fillWorkflowStateUpdated()
+fillWorkflowStateUpdated()
