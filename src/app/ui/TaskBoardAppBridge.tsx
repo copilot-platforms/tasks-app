1dd096000c177d4f06839fa4ecb60b2a6266a7ae
Rojan Rajbhandari <rrojan20@gmail.com>
2025-01-09 19:08:18 +0545

    feat(OUT-1254): implement app bridge for tasks board

---
+++ src/app/ui/TaskBoardAppBridge.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import { Icons } from '@/hooks/app-bridge/types'
+import { useActionsMenu } from '@/hooks/app-bridge/useActionsMenu'
+import { usePrimaryCta } from '@/hooks/app-bridge/usePrimaryCta'
+import { setShowModal } from '@/redux/features/createTaskSlice'
+import store from '@/redux/store'
+import { UserRole } from '@api/core/types/user'
+import { useRouter } from 'next/navigation'
+
+interface TaskBoardAppBridgeProps {
+  token: string
+  role: UserRole
+  portalUrl?: string
+}
+
+export const TaskBoardAppBridge = ({ token, role, portalUrl }: TaskBoardAppBridgeProps) => {
+  const router = useRouter()
+
+  const handleTaskCreate = () => {
+    store.dispatch(setShowModal())
+  }
+  const handleManageTemplatesClick = () => {
+    router.push(`/manage-templates?token=${token}`)
+  }
+
+  usePrimaryCta(
+    {
+      label: 'Create task',
+      icon: Icons.PLUS,
+      onClick: handleTaskCreate,
+    },
+    { portalUrl },
+  )
+
+  useActionsMenu(
+    [
+      {
+        label: 'Manage templates',
+        icon: Icons.ARCHIVE,
+        onClick: handleManageTemplatesClick,
+      },
+    ],
+    { portalUrl },
+  )
+
+  return <></>
+}
