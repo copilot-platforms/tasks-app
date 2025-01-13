64e0253ddab986651eb77089fd74cbd3f1de318a
Rojan Rajbhandari <rrojan20@gmail.com>
2025-01-13 15:41:51 +0545

    fix(OUT-1256): remove menubox from client view

--- src/app/detail/ui/MenuBoxContainer.tsx
+++ src/app/detail/ui/MenuBoxContainer.tsx
@@ -1,13 +1,11 @@
 'use client'
 
+import { UserRole } from '@/app/api/core/types/user'
 import { ListBtn } from '@/components/buttons/ListBtn'
-import { MoreBtn } from '@/components/buttons/MoreBtn'
 import { MenuBox } from '@/components/inputs/MenuBox'
 import { TrashIcon } from '@/icons'
 import { setShowConfirmDeleteModal } from '@/redux/features/taskDetailsSlice'
 import store from '@/redux/store'
-import { DetailAppBridge } from './DetailAppBridge'
-import { UserRole } from '@/app/api/core/types/user'
 
 interface MenuBoxContainerProps {
   role: UserRole
@@ -15,7 +13,7 @@ interface MenuBoxContainerProps {
 }
 
 export const MenuBoxContainer = ({ role, isPreviewMode }: MenuBoxContainerProps) => {
-  if (role === UserRole.Client && !isPreviewMode) {
+  if ((role === UserRole.IU && !isPreviewMode) || role === UserRole.Client) {
     return null
   }
 
