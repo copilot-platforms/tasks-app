ec7538c97fd03550c8d66c28461db890e923b998
Rojan Rajbhandari <rrojan20@gmail.com>
2025-01-13 17:20:30 +0545

    feat(OUT-1263): temporary accomodation for sidebar toggle button

--- src/app/detail/ui/ToggleButtonContainer.tsx
+++ src/app/detail/ui/ToggleButtonContainer.tsx
@@ -1,10 +1,20 @@
 'use client'
 import { ToggleBtn } from '@/components/buttons/ToggleBtn'
+import { selectTaskBoard } from '@/redux/features/taskBoardSlice'
 import { selectTaskDetails, setShowSidebar } from '@/redux/features/taskDetailsSlice'
 import store from '@/redux/store'
+import { Box } from '@mui/material'
 import { useSelector } from 'react-redux'
 
 export const ToggleButtonContainer = () => {
   const { showSidebar } = useSelector(selectTaskDetails)
+  const { previewMode } = useSelector(selectTaskBoard)
+  if (!previewMode) {
+    return (
+      <Box sx={{ width: '72vw', display: 'flex', justifyContent: 'flex-end' }}>
+        <ToggleBtn onClick={() => store.dispatch(setShowSidebar(!showSidebar))} />
+      </Box>
+    )
+  }
   return <ToggleBtn onClick={() => store.dispatch(setShowSidebar(!showSidebar))} />
 }
