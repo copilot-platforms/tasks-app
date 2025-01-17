5dc80f74c456bb494326d38b11614e7d08c4bf2c
Arpan Dhakal <arpandhakal20@gmail.com>
2025-01-17 15:11:53 +0545

    tryfix(OUT-1293) : added 0 seconds delay on headerBreadcrumbs

--- src/components/layouts/HeaderBreadcrumbs.tsx
+++ src/components/layouts/HeaderBreadcrumbs.tsx
@@ -8,6 +8,7 @@ import { selectTaskBoard } from '@/redux/features/taskBoardSlice'
 import { UserType } from '@/types/interfaces'
 import { Stack, Typography } from '@mui/material'
 import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 import { useSelector } from 'react-redux'
 
 type ValidTasksBoardLink = '/' | '/client'
@@ -25,6 +26,11 @@ export const HeaderBreadcrumbs = ({
 }) => {
   const { previewMode } = useSelector(selectTaskBoard)
   const router = useRouter()
+
+  const [awake, setAwake] = useState(false)
+  setTimeout(() => {
+    setAwake(true)
+  }, 0)
 
   const getTasksLink = (userType: UserType): ValidTasksBoardLink => {
     if (previewMode) return '/client'
