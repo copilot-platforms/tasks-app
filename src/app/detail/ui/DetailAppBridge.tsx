0623f84db8e4b9254bed16a1ebc500b5caac8e12
Arpan Dhakal <arpandhakal20@gmail.com>
2025-01-17 16:16:09 +0545

    tryfix(OUT-1293) : useBreadCrumbs used in detailAppBridge

--- src/app/detail/ui/DetailAppBridge.tsx
+++ src/app/detail/ui/DetailAppBridge.tsx
@@ -2,10 +2,13 @@
 
 import { Clickable, Icons } from '@/hooks/app-bridge/types'
 import { useActionsMenu } from '@/hooks/app-bridge/useActionsMenu'
+import { useBreadcrumbs } from '@/hooks/app-bridge/useBreadcrumbs'
 import { usePrimaryCta } from '@/hooks/app-bridge/usePrimaryCta'
 import { useSecondaryCta } from '@/hooks/app-bridge/useSecondaryCta'
+import { selectTaskBoard } from '@/redux/features/taskBoardSlice'
 import { setShowConfirmDeleteModal } from '@/redux/features/taskDetailsSlice'
 import store from '@/redux/store'
+import { useSelector } from 'react-redux'
 
 interface DetailAppBridgeProps {
   isArchived: boolean
@@ -15,6 +18,7 @@ interface DetailAppBridgeProps {
 
 export const DetailAppBridge = ({ isArchived, handleToggleArchive, portalUrl }: DetailAppBridgeProps) => {
   const handleDelete = () => store.dispatch(setShowConfirmDeleteModal())
+  const { activeTask } = useSelector(selectTaskBoard)
 
   usePrimaryCta(null, { portalUrl })
   useSecondaryCta(
@@ -25,6 +29,15 @@ export const DetailAppBridge = ({ isArchived, handleToggleArchive, portalUrl }: 
           onClick: handleToggleArchive,
         }
       : null,
+    { portalUrl },
+  )
+
+  useBreadcrumbs(
+    [
+      {
+        label: activeTask?.title ?? '',
+      },
+    ],
     { portalUrl },
   )
 
--- src/components/layouts/HeaderBreadcrumbs.tsx
+++ src/components/layouts/HeaderBreadcrumbs.tsx
@@ -38,10 +38,6 @@ export const HeaderBreadcrumbs = ({
   useBreadcrumbs(
     [
       {
-        label: 'Tasks',
-        onClick: () => router.push(getTasksLink(userType) + `?token=${token}`),
-      },
-      {
         label: title,
       },
     ],
