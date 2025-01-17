bc9a42fbc027a58818a838c042a67e4ed97daaac
Arpan Dhakal <arpandhakal20@gmail.com>
2025-01-17 14:30:45 +0545

    tryfix(OUT-1293) : testing for problems on useBreadcrumbs

--- src/hooks/app-bridge/useBreadcrumbs.tsx
+++ src/hooks/app-bridge/useBreadcrumbs.tsx
@@ -21,7 +21,7 @@ export const useBreadcrumbs = (breadcrumbs: Clickable[], config?: Configurable) 
       })),
     }
 
-    window.parent.postMessage(payload, ensureHttps(config?.portalUrl ?? 'https://dashboard.copilot.com'))
+    window.parent.postMessage(payload, ensureHttps('https://dashboard.copilot.com'))
     if (config?.portalUrl) {
       window.parent.postMessage(payload, ensureHttps(config.portalUrl))
     }
