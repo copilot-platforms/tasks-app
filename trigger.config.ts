ab4c213982970c37807ee00f535c02488f67c952
Rojan Rajbhandari <rrojan20@gmail.com>
2025-01-29 17:51:08 +0545

    feat(trigger): initialize trigger config

---
+++ trigger.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from '@trigger.dev/sdk/v3'
+
+const project = process.env.TRIGGER_PROJECT
+if (!project) {
+  throw new Error('Could not find project name for Trigger.dev in environment')
+}
+
+export default defineConfig({
+  project,
+  runtime: 'node',
+  logLevel: 'log',
+  // The max compute seconds a task is allowed to run. If the task run exceeds this duration, it will be stopped.
+  // You can override this on an individual task.
+  // See https://trigger.dev/docs/runs/max-duration
+  maxDuration: 3600,
+  retries: {
+    enabledInDev: true,
+    default: {
+      maxAttempts: 3,
+      minTimeoutInMs: 1000,
+      maxTimeoutInMs: 10000,
+      factor: 2,
+      randomize: true,
+    },
+  },
+  dirs: ['./src/triggers'],
+})
