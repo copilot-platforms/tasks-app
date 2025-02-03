bbcd9519aebd0fa7363f40b7fd1e09c1b8b9b4a8
Rojan Rajbhandari <rrojan20@gmail.com>
2025-02-03 17:45:52 +0545

    feat(OUT-1363): add clever delay to check for trigger / triggerAndWait functionality

--- src/triggers/health-check.ts
+++ src/triggers/health-check.ts
@@ -1,10 +1,13 @@
-import { logger, task } from '@trigger.dev/sdk/v3'
+import { logger, task, wait } from '@trigger.dev/sdk/v3'
 
 export const healthCheck = task({
   id: 'health-check',
   maxDuration: 60,
   run: async (payload: any, { ctx }) => {
     logger.log('Checking health of trigger.dev tasks worker', { payload, ctx })
+
+    // Check whether trigger, triggerAndWait, triggerAndPoll, etc work properly or not
+    await wait.for({ seconds: 30 })
 
     return {
       message: 'Trigger.dev task workers are rolling!',
