0157f435e180f0d075356d2f9dc85e7682f20aa5
Rojan Rajbhandari <rrojan20@gmail.com>
2025-02-03 17:40:23 +0545

    feat(OUT-1363): modify health-check endpoint to check db & trigger.dev conn status

--- src/app/api/health-check/route.ts
+++ src/app/api/health-check/route.ts
@@ -1,7 +1,28 @@
+import DBClient from '@/lib/db'
+import { healthCheck } from '@/triggers/health-check'
+import { tasks } from '@trigger.dev/sdk/v3'
 import { NextResponse } from 'next/server'
 
 export async function GET() {
+  let dbConnection: boolean = false
+  let triggerConnection: boolean = false
+
+  // Check database connection
+  try {
+    const client = DBClient.getInstance()
+    await client.$queryRaw`SELECT 1`
+    dbConnection = true
+  } catch {}
+
+  // Check trigger.dev workers connection
+  try {
+    const queueRunHandler = await tasks.trigger<typeof healthCheck>('health-check', {})
+    triggerConnection = !!queueRunHandler?.id.startsWith('run')
+  } catch {}
+
   return NextResponse.json({
-    message: 'Copilot Tasks app API is rolling 🔥',
+    message: 'Copilot Tasks App API is rolling 🔥',
+    dbConnection,
+    triggerConnection,
   })
 }
