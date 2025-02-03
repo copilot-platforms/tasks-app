1f58fc41d38d26a8c0cab8715c56cc3311651ba5
Rojan Rajbhandari <rrojan20@gmail.com>
2025-02-03 17:39:39 +0545

    feat(OUT-1363): add health-check task to check that trigger workers are working

---
+++ src/triggers/health-check.ts
@@ -0,0 +1,13 @@
+import { logger, task } from '@trigger.dev/sdk/v3'
+
+export const healthCheck = task({
+  id: 'health-check',
+  maxDuration: 60,
+  run: async (payload: any, { ctx }) => {
+    logger.log('Checking health of trigger.dev tasks worker', { payload, ctx })
+
+    return {
+      message: 'Trigger.dev task workers are rolling!',
+    }
+  },
+})
