90b6b055602dab0bef0b36e9f261decbcd3e6cf1
Rojan Rajbhandari <rrojan20@gmail.com>
2025-02-07 14:33:16 +0545

    feat(OUT-1352): implement task update notifications

---
+++ src/jobs/notifications/send-task-update-notifications.ts
@@ -0,0 +1,27 @@
+import { TaskNotificationsService } from '@/app/api/tasks/task-notifications.service'
+import { TaskWithWorkflowState } from '@/types/db'
+import User from '@api/core/models/User.model'
+import { logger, task } from '@trigger.dev/sdk/v3'
+
+type UpdateTaskNotificationPayload = {
+  user: User
+  prevTask: TaskWithWorkflowState
+  updatedTask: TaskWithWorkflowState
+}
+
+export const sendTaskUpdateNotifications = task({
+  id: 'send-task-create-notifications',
+  machine: { preset: 'medium-1x' },
+
+  run: async (payload: UpdateTaskNotificationPayload, { ctx }) => {
+    logger.log('Sending task update notifications for:', { payload, ctx })
+
+    const { prevTask, updatedTask, user } = payload
+    const taskNotificationsSevice = new TaskNotificationsService(user)
+    await taskNotificationsSevice.sendTaskUpdateNotifications(prevTask, updatedTask)
+
+    return {
+      message: `Sent update notifications for taskId ${updatedTask.id} successfully`,
+    }
+  },
+})
