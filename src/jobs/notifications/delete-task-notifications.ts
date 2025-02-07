3d38544429ab62d5e7f1330602681218ae94c2a2
Rojan Rajbhandari <rrojan20@gmail.com>
2025-02-07 15:46:45 +0545

    feat(OUT-1376): implement notification deletion on task delete

---
+++ src/jobs/notifications/delete-task-notifications.ts
@@ -0,0 +1,26 @@
+import { TaskNotificationsService } from '@/app/api/tasks/task-notifications.service'
+import { TaskWithWorkflowState } from '@/types/db'
+import User from '@api/core/models/User.model'
+import { logger, task } from '@trigger.dev/sdk/v3'
+
+type DeleteTaskNotificationPayload = {
+  user: User
+  task: TaskWithWorkflowState
+}
+
+export const deleteTaskNotifications = task({
+  id: 'delete-task-notifications',
+  machine: { preset: 'small-2x' },
+
+  run: async (payload: DeleteTaskNotificationPayload, { ctx }) => {
+    logger.log('Deleting task notifications for:', { payload, ctx })
+
+    const { task, user } = payload
+    const taskNotificationsSevice = new TaskNotificationsService(user)
+    await taskNotificationsSevice.removeDeletedTaskNotifications(task)
+
+    return {
+      message: `Deleted notifications for taskId ${task.id} successfully`,
+    }
+  },
+})
