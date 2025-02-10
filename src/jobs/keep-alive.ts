3347c5f8371f0c6b957ae00bf5370564ad753f41
Rojan Rajbhandari <rrojan20@gmail.com>
2025-02-10 14:08:25 +0545

    chore: schedule keep-alive to run every 5 mins

--- src/jobs/keep-alive.ts
+++ src/jobs/keep-alive.ts
@@ -1,11 +1,11 @@
 import { schedules } from '@trigger.dev/sdk/v3'
 
+// This task does nothing except warm up our workers and ensure it stays active.
 export const keepAlive = schedules.task({
   id: 'keep-alive',
   maxDuration: 10,
-  cron: '* * * * *',
+  cron: '*/5 * * * *', // Run every 5 minutes
   run: async () => {
-    // This task does nothing except warm up our workers and ensure it stays active.
     return { message: 'Worker is warm' }
   },
 })
