4cd40935bffa9eee57aafea0ec79b9b3e8da8ca9
Arpan Dhakal <arpandhakal20@gmail.com>
2025-02-13 12:48:20 +0545

    feat(OUT-1400) : transferred comment create notification to a trigger job

--- src/jobs/notifications/index.ts
+++ src/jobs/notifications/index.ts
@@ -1,3 +1,4 @@
 export { deleteTaskNotifications } from './delete-task-notifications'
 export { sendTaskCreateNotifications } from './send-task-create-notifications'
 export { sendTaskUpdateNotifications } from './send-task-update-notifications'
+export { sendCommentCreateNotifications } from './send-comment-create-notifications'
