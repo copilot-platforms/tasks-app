64d192d5605b76ff667cc0982366148074284b7d
Rojan Rajbhandari <rrojan20@gmail.com>
2025-02-21 15:40:10 +0545

    feat(OUT-1420): add wrapper Tooltip component

---
+++ src/components/atoms/Tooltip.tsx
@@ -0,0 +1,29 @@
+import { Tooltip } from '@mui/material'
+import { ReactElement } from 'react'
+
+interface TooltipProps {
+  title: string
+  children: ReactElement
+}
+
+const TooltipComponent = ({ title, children }: TooltipProps) => {
+  return (
+    <Tooltip
+      title={title}
+      slotProps={{
+        tooltip: {
+          sx: {
+            fontWeight: 500,
+            fontSize: '12px',
+            color: 'rgb(107, 111, 118)',
+            backgroundColor: 'white',
+          },
+        },
+      }}
+    >
+      {children}
+    </Tooltip>
+  )
+}
+
+export { TooltipComponent as Tooltip }
