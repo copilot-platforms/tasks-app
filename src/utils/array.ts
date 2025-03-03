e34b31e50e41c8b3897db37124bb4858e4d48f66
Rojan Rajbhandari <roj@Rojans-MacBook-Air.local>
2025-03-03 13:29:33 +0545

    chore(OUT-1457): add util to get array intersection

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -4,3 +4,11 @@
 export const getArrayDifference = <T>(arr1: T[], arr2: T[]): T[] => {
   return arr1.filter((item) => !arr2.includes(item))
 }
+
+/**
+ * Gets the intersection between two arrays
+ */
+export const getArrayIntersection = <T>(arr1: T[], arr2: T[]): T[] => {
+  const set2 = new Set(arr2)
+  return arr1.filter((item) => set2.has(item))
+}
