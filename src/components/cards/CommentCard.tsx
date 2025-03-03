bc42ae32cbbafbb57890e9b95db1872df387e378
Arpan Dhakal <arpandhakal20@gmail.com>
2025-03-03 18:22:40 +0545

    feat(OUT-1475,OUT-1476) : replies creation and view
    - created ui to view replies.
    - created ui and functionality to post replies.
    - code refactoring for commentInput, attachmentLayout to make them cleaner.

--- src/components/cards/CommentCard.tsx
+++ src/components/cards/CommentCard.tsx
@@ -8,7 +8,13 @@ import { z } from 'zod'
 
 import { updateComment } from '@/app/detail/[task_id]/[user_type]/actions'
 import { DotSeparator } from '@/app/detail/ui/DotSeparator'
-import { BoldTypography, CommentCardContainer, StyledModal, StyledTypography } from '@/app/detail/ui/styledComponent'
+import {
+  BoldTypography,
+  CommentCardContainer,
+  CustomDivider,
+  StyledModal,
+  StyledTypography,
+} from '@/app/detail/ui/styledComponent'
 import AttachmentLayout from '@/components/AttachmentLayout'
 import { ListBtn } from '@/components/buttons/ListBtn'
 import { PrimaryBtn } from '@/components/buttons/PrimaryBtn'
@@ -30,16 +36,10 @@ import { getMentionsList } from '@/utils/getMentionList'
 import { getTimeDifference } from '@/utils/getTimeDifference'
 import { deleteEditorAttachmentsHandler, uploadImageHandler } from '@/utils/inlineImage'
 import { isTapwriteContentEmpty } from '@/utils/isTapwriteContentEmpty'
-import { commentAddedResponseSchema } from '@api/activity-logs/schemas/CommentAddedSchema'
+import { commentAddedResponseSchema, ReplyResponse } from '@api/activity-logs/schemas/CommentAddedSchema'
 import { LogResponse } from '@api/activity-logs/schemas/LogResponseSchema'
-
-const CustomDivider = styled(Box)(({ theme }) => ({
-  height: '1px',
-  width: 'calc(100% + 20px)',
-  backgroundColor: theme.color.borders.border,
-  marginLeft: '-10px',
-  marginRight: '-10px',
-}))
+import { ReplyCard } from '@/components/cards/ReplyCard'
+import { ReplyInput } from '@/components/inputs/ReplyInput'
 
 export const CommentCard = ({
   comment,
@@ -52,9 +52,9 @@ export const CommentCard = ({
   deleteComment: (id: string) => void
   task_id: string
 }) => {
-  const [showReply, setShowReply] = useState(false)
+  const [showReply, setShowReply] = useState(true)
   const [isHovered, setIsHovered] = useState(false)
-  const [detail, setDetail] = useState('')
+
   const [timeAgo, setTimeAgo] = useState(getTimeDifference(comment.createdAt))
   const [isReadOnly, setIsReadOnly] = useState<boolean>(true)
   const editRef = useRef<HTMLDivElement>(null)
@@ -79,19 +79,6 @@ export const CommentCard = ({
   const content = (comment.details as { content: string }).content || ''
   const [editedContent, setEditedContent] = useState(content)
   const [isListOrMenuActive, setIsListOrMenuActive] = useState(false)
-
-  const handleReplySubmission = () => {
-    const replyPayload: CreateComment = {
-      content: detail,
-      taskId: task_id,
-      parentId: commentAddedResponseSchema.parse(comment.details).id,
-      mentions: getMentionsList(detail),
-    }
-    if (detail) {
-      createComment(replyPayload)
-      setDetail('')
-    }
-  }
 
   useEffect(() => {
     const updateTimeAgo = () => setTimeAgo(getTimeDifference(comment.createdAt))
@@ -161,168 +148,112 @@ export const CommentCard = ({
         overflow: 'hidden',
       }}
     >
-      <Stack direction="column" rowGap={'2px'}>
-        {isReadOnly && (
-          <Stack direction="row" justifyContent={'space-between'} alignItems="center">
-            <Stack direction="row" columnGap={1} alignItems="center">
-              {commentUser ? (
-                <BoldTypography>{getAssigneeName(commentUser, '')}</BoldTypography>
-              ) : (
-                <Typography variant="md" sx={{ fontStyle: 'italic' }}>
-                  Deleted User
-                </Typography>
-              )}
-              <DotSeparator />
-              <StyledTypography sx={{ lineHeight: '22px' }}>
-                {timeAgo} {comment.details.updatedAt !== comment.details.createdAt ? '(edited)' : ''}
-              </StyledTypography>
-            </Stack>
+      <Stack direction="column" rowGap={'4px'}>
+        <Stack direction="column" rowGap={'2px'} sx={{ paddingBottom: '4px' }}>
+          {isReadOnly && (
+            <Stack direction="row" justifyContent={'space-between'} alignItems="center">
+              <Stack direction="row" columnGap={1} alignItems="center">
+                {commentUser ? (
+                  <BoldTypography>{getAssigneeName(commentUser, '')}</BoldTypography>
+                ) : (
+                  <Typography variant="md" sx={{ fontStyle: 'italic' }}>
+                    Deleted User
+                  </Typography>
+                )}
+                <DotSeparator />
+                <StyledTypography sx={{ lineHeight: '22px' }}>
+                  {timeAgo} {comment.details.updatedAt !== comment.details.createdAt ? '(edited)' : ''}
+                </StyledTypography>
+              </Stack>
 
-            {(isHovered || isMobile() || isMenuOpen) && canEdit && (
-              <Stack direction="row" columnGap={2} sx={{ height: '10px' }} alignItems="center">
-                <MenuBox
-                  getMenuOpen={(open) => {
-                    setIsMenuOpen(open)
-                  }}
-                  menuContent={
-                    <>
-                      <ListBtn
-                        content="Edit comment"
-                        handleClick={() => {
-                          setIsReadOnly(false)
-                          if (editRef.current) {
-                            editRef.current.focus()
-                          }
-                        }}
-                        icon={<PencilIcon />}
-                        contentColor={(theme) => theme.color.text.text}
-                        width="175px"
-                        height="33px"
-                      />
-                      {canDelete && (
+              {(isHovered || isMobile() || isMenuOpen) && canEdit && (
+                <Stack direction="row" columnGap={2} sx={{ height: '10px' }} alignItems="center">
+                  <MenuBox
+                    getMenuOpen={(open) => {
+                      setIsMenuOpen(open)
+                    }}
+                    menuContent={
+                      <>
                         <ListBtn
-                          content="Delete comment"
+                          content="Edit comment"
                           handleClick={() => {
-                            setShowConfirmDeleteModal(true)
+                            setIsReadOnly(false)
+                            if (editRef.current) {
+                              editRef.current.focus()
+                            }
                           }}
-                          icon={<TrashIcon />}
-                          contentColor={(theme) => theme.color.error}
+                          icon={<PencilIcon />}
+                          contentColor={(theme) => theme.color.text.text}
                           width="175px"
                           height="33px"
                         />
-                      )}
-                    </>
-                  }
-                  isSecondary
-                  width={'22px'}
-                  height={'22px'}
-                  displayButtonBackground={false}
-                  noHover={false}
-                />
-              </Stack>
-            )}
-          </Stack>
-        )}
-        <Box onBlur={() => setIsFocused(false)} onFocus={() => setIsFocused(true)}>
-          <Tapwrite
-            content={editedContent}
-            onActiveStatusChange={(prop) => {
-              const { isListActive, isFloatingMenuActive } = prop
-              setIsListOrMenuActive(isListActive || isFloatingMenuActive)
-            }}
-            getContent={setEditedContent}
-            readonly={isReadOnly}
-            editorRef={editRef}
-            editorClass={'tapwrite-comment'}
-            addAttachmentButton={!isReadOnly}
-            uploadFn={uploadFn}
-            deleteEditorAttachments={(url) => deleteEditorAttachmentsHandler(url, token ?? '', task_id, null)}
-            maxUploadLimit={MAX_UPLOAD_LIMIT}
-            attachmentLayout={(props) => <AttachmentLayout {...props} isComment={true} />}
-            hardbreak
-            parentContainerStyle={{
-              width: '100%',
-              maxWidth: '100%',
-              display: 'flex',
-              flexDirection: 'column',
-              overflow: 'hidden',
-              wordBreak: 'break-word',
-              whiteSpace: 'pre-wrap',
-            }}
-            endButtons={<EditCommentButtons cancelEdit={cancelEdit} handleEdit={handleEdit} isReadOnly={isReadOnly} />}
-            handleImageDoubleClick={handleImagePreview}
-          />
-        </Box>
+                        {canDelete && (
+                          <ListBtn
+                            content="Delete comment"
+                            handleClick={() => {
+                              setShowConfirmDeleteModal(true)
+                            }}
+                            icon={<TrashIcon />}
+                            contentColor={(theme) => theme.color.error}
+                            width="175px"
+                            height="33px"
+                          />
+                        )}
+                      </>
+                    }
+                    isSecondary
+                    width={'22px'}
+                    height={'22px'}
+                    displayButtonBackground={false}
+                    noHover={false}
+                  />
+                </Stack>
+              )}
+            </Stack>
+          )}
+          <Box onBlur={() => setIsFocused(false)} onFocus={() => setIsFocused(true)}>
+            <Tapwrite
+              content={editedContent}
+              onActiveStatusChange={(prop) => {
+                const { isListActive, isFloatingMenuActive } = prop
+                setIsListOrMenuActive(isListActive || isFloatingMenuActive)
+              }}
+              getContent={setEditedContent}
+              readonly={isReadOnly}
+              editorRef={editRef}
+              editorClass={'tapwrite-comment'}
+              addAttachmentButton={!isReadOnly}
+              uploadFn={uploadFn}
+              deleteEditorAttachments={(url) => deleteEditorAttachmentsHandler(url, token ?? '', task_id, null)}
+              maxUploadLimit={MAX_UPLOAD_LIMIT}
+              attachmentLayout={(props) => <AttachmentLayout {...props} isComment={true} />}
+              hardbreak
+              parentContainerStyle={{
+                width: '100%',
+                maxWidth: '100%',
+                display: 'flex',
+                flexDirection: 'column',
+                overflow: 'hidden',
+                wordBreak: 'break-word',
+                whiteSpace: 'pre-wrap',
+              }}
+              endButtons={<EditCommentButtons cancelEdit={cancelEdit} handleEdit={handleEdit} isReadOnly={isReadOnly} />}
+              handleImageDoubleClick={handleImagePreview}
+            />
+          </Box>
+        </Stack>
+
+        <CustomDivider />
 
         {Array.isArray((comment as LogResponse).details?.replies) &&
-          ((comment as LogResponse).details.replies as LogResponse[]).map((item: LogResponse) => {
-            return (
-              <Stack direction="column" rowGap={3} key={item.id}>
-                <CustomDivider />
-                <Stack direction="row" columnGap={2} alignItems={'center'}>
-                  <Avatar
-                    alt={item?.initiator?.givenName}
-                    src={item?.initiator?.avatarImageUrl || 'user'}
-                    sx={{ width: '20px', height: '20px', fontSize: '14px' }}
-                  />
-                  <BoldTypography>
-                    {item.initiator?.givenName} {item.initiator?.familyName}
-                  </BoldTypography>
-                  <StyledTypography> {getTimeDifference(item.createdAt)}</StyledTypography>
-                </Stack>
-                <Tapwrite
-                  content={item.details?.content as string}
-                  getContent={() => {}}
-                  readonly
-                  editorClass="tapwrite-comment"
-                  attachmentLayout={AttachmentLayout}
-                  parentContainerStyle={{
-                    width: '100%',
-                    height: '100%',
-                    maxWidth: '566px',
-                    display: 'flex',
-                    flexDirection: 'column',
-                  }}
-                />
-              </Stack>
-            )
+          ((comment as LogResponse).details.replies as ReplyResponse[]).map((item: ReplyResponse) => {
+            return <ReplyCard item={item} key={item.id} />
           })}
 
         {(Array.isArray((comment as LogResponse).details?.replies) &&
           ((comment as LogResponse).details.replies as LogResponse[]).length > 0) ||
         showReply ? (
-          <>
-            <CustomDivider />
-            <Stack direction="row" columnGap={1} alignItems="flex-start">
-              <Avatar alt="user" src={''} sx={{ width: '20px', height: '20px', marginTop: '5px' }} />
-              <Tapwrite
-                content={detail}
-                getContent={setDetail}
-                placeholder="Leave a reply..."
-                // suggestions={assigneeSuggestions}
-                editorClass="tapwrite-reply-input"
-                attachmentLayout={AttachmentLayout}
-                parentContainerStyle={{
-                  width: '100%',
-                  height: '100%',
-                  maxWidth: '566px',
-                  display: 'flex',
-                  flexDirection: 'column',
-                }}
-              />
-              <InputAdornment
-                position="end"
-                sx={{
-                  alignSelf: 'flex-end',
-                  display: 'flex',
-                  alignItems: 'flex-end',
-                  paddingBottom: '10px',
-                }}
-              >
-                <PrimaryBtn buttonText="Reply" handleClick={handleReplySubmission} />
-              </InputAdornment>
-            </Stack>
-          </>
+          <ReplyInput comment={comment} task_id={task_id} createComment={createComment} uploadFn={uploadFn} />
         ) : null}
       </Stack>
       <StyledModal
