64ecfe52727be27c8ef0a1dae4d5f120c2db834f
Rojan Rajbhandari <roj@Rojans-MacBook-Air.local>
2025-03-03 13:29:56 +0545

    feat(OUT-1457): add support for `expandComments` search param

--- src/app/api/tasks/[id]/activity-logs/activity.controller.ts
+++ src/app/api/tasks/[id]/activity-logs/activity.controller.ts
@@ -1,14 +1,16 @@
-import { NextRequest, NextResponse } from 'next/server'
-import authenticate from '@api/core/utils/authenticate'
+import { getSearchParams } from '@/utils/request'
 import { ActivityLogService } from '@api/activity-logs/services/activity-log.service'
 import { IdParams } from '@api/core/types/api'
+import authenticate from '@api/core/utils/authenticate'
 import httpStatus from 'http-status'
+import { NextRequest, NextResponse } from 'next/server'
 
 export const get = async (req: NextRequest, { params: { id } }: IdParams) => {
   const user = await authenticate(req)
 
+  const { expandComments } = getSearchParams(req.nextUrl.searchParams, ['expandComments'])
   const activityLogService = new ActivityLogService(user)
-  const logs = await activityLogService.get(id)
+  const logs = await activityLogService.get(id, { expandComments: expandComments?.split(',') })
 
   return NextResponse.json(
     {
