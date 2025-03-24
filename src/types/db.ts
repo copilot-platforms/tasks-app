7bb56c0ed12def24b057a7114012adca886f7b4d
Rojan Rajbhandari <rrojan20@gmail.com>
2025-03-24 15:15:51 +0545

    refactor(OUT-1569): clean code

--- src/types/db.ts
+++ src/types/db.ts
@@ -1,5 +1,3 @@
 import { Task, WorkflowState } from '@prisma/client'
 
 export type TaskWithWorkflowState = Task & { workflowState: WorkflowState }
-
-export type TaskWithPath = Task & { path: string }
