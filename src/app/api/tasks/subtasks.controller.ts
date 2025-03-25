ed48478082f7dd58c3afa712e0c01d0f28afd825
Rojan Rajbhandari <rrojan20@gmail.com>
2025-03-25 12:42:44 +0545

    feat(OUT-1570): implement route to get number of subtasks for a particular taskId

---
+++ src/app/api/tasks/subtasks.controller.ts
@@ -0,0 +1,11 @@
+import { IdParams } from '@api/core/types/api'
+import authenticate from '@api/core/utils/authenticate'
+import { SubtaskService } from '@api/tasks/subtasks.service'
+import { NextRequest, NextResponse } from 'next/server'
+
+export const getSubtaskCount = async (req: NextRequest, { params: { id } }: IdParams) => {
+  const user = await authenticate(req)
+  const subtaskService = new SubtaskService(user)
+  const count = await subtaskService.getSubtaskCounts(id)
+  return NextResponse.json({ count })
+}
