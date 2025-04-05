4f1579a09f514bc675005fd53614ed9cde67a495
Rojan Rajbhandari <rrojan20@gmail.com>
2025-04-05 19:57:26 +0545

    refactor(OUT-1608): clean proptypes

--- src/app/_fetchers/WorkflowStateFetcher.tsx
+++ src/app/_fetchers/WorkflowStateFetcher.tsx
@@ -1,14 +1,13 @@
 export const fetchCache = 'force-no-store'
 
 import { apiUrl } from '@/config'
-import { WorkflowStateResponse } from '@/types/dto/workflowStates.dto'
 import { ClientSideStateUpdate } from '@/hoc/ClientSideStateUpdate'
-import { ReactNode } from 'react'
 import { TaskResponse } from '@/types/dto/tasks.dto'
+import { WorkflowStateResponse } from '@/types/dto/workflowStates.dto'
+import { PropsWithToken } from '@/types/interfaces'
+import { PropsWithChildren } from 'react'
 
-interface Props {
-  token: string
-  children: ReactNode
+interface Props extends PropsWithToken, PropsWithChildren {
   task?: TaskResponse
 }
 
