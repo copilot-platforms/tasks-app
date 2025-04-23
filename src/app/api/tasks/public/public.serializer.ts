455e85e2229c2e9f6b90d4a827875565a496265e
Rojan Rajbhandari <rrojan20@gmail.com>
2025-04-23 14:55:51 +0545

    feat(OUT-1671): add rough serializer for public tasks

---
+++ src/app/api/tasks/public/public.serializer.ts
@@ -0,0 +1,43 @@
+import { RFC3339DateSchema } from '@/types/common'
+import { toRFC3339 } from '@/utils/dateHelper'
+import { PublicTaskDto, PublicTaskDtoSchema } from '@api/tasks/public/public.dto'
+import { Task, WorkflowState } from '@prisma/client'
+
+const statusMap: Record<WorkflowState['type'], PublicTaskDto['status']> = {
+  backlog: 'todo',
+  unstarted: 'todo',
+  started: 'inProgress',
+  completed: 'completed',
+  cancelled: 'todo',
+}
+
+export class PublicTaskSerializer {
+  static serialize(task: Task & { workflowState: WorkflowState }): PublicTaskDto {
+    return PublicTaskDtoSchema.parse({
+      id: task.id,
+      object: 'task',
+      name: task.title,
+      description: task.body || '',
+      parentTaskId: task.parentId,
+      assigneeId: task.assigneeId,
+      assigneeType: task.assigneeType,
+      dueDate: toRFC3339(task.dueDate),
+      label: task.label,
+      status: statusMap[task.workflowState.type],
+      createdBy: task.createdById,
+      completedDate: toRFC3339(task.completedAt),
+      creatorType: 'internalUser',
+      createdDate: RFC3339DateSchema.parse(toRFC3339(task.createdAt)),
+      isArchived: task.isArchived,
+      archivedDate: toRFC3339(task.lastArchivedDate),
+      isDeleted: task.deletedAt ? true : false,
+      deletedDate: toRFC3339(task.deletedAt),
+      source: 'web', // Placeholder for now
+    })
+  }
+
+  static deserialize() {
+    // TODO: Implement while implementing public create endpoint
+    return {}
+  }
+}
