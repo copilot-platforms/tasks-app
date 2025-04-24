0887936a8851ed0a0cbe89bf35466b86f82fc684
Arpan Dhakal <arpandhakal20@gmail.com>
2025-04-24 13:48:53 +0545

    fix(OUT-1679) : applied token based pagination, code refactoring for getAllTasktemplates public api

--- src/app/api/tasks/templates/public/public.controller.ts
+++ src/app/api/tasks/templates/public/public.controller.ts
@@ -2,14 +2,23 @@ import { IdParams } from '@/app/api/core/types/api'
 import authenticate from '@/app/api/core/utils/authenticate'
 import { PublicTemplateSerializer } from '@/app/api/tasks/templates/public/public.serializer'
 import { TemplatesService } from '@/app/api/tasks/templates/templates.service'
+import { getSearchParams } from '@/utils/request'
+import { encode, decode } from 'js-base64'
+
 import { NextRequest, NextResponse } from 'next/server'
 
 export const getTaskTemplatesPublic = async (req: NextRequest) => {
   const user = await authenticate(req)
-  const nextToken = req.nextUrl.searchParams.get('nextToken') || undefined // implement token based pagination here
+  const { limit, nextToken } = getSearchParams(req.nextUrl.searchParams, ['limit', 'nextToken'])
   const templateService = new TemplatesService(user)
-  const templates = await templateService.getTaskTemplates()
-  return NextResponse.json({ data: PublicTemplateSerializer.serialize(templates) })
+  const templates = await templateService.getTaskTemplates({
+    limit: Number(limit),
+    lastIdCursor: nextToken ? decode(nextToken) : undefined,
+  })
+  const lastTemplateId = templates[templates.length - 1]?.id
+  const hasMoreTemplates = lastTemplateId ? await templateService.hasMoreTemplatesAfterCursor(lastTemplateId) : false
+  const base64NextToken = hasMoreTemplates ? encode(lastTemplateId) : undefined
+  return NextResponse.json({ data: PublicTemplateSerializer.serialize(templates), nextToken: base64NextToken })
 }
 
 export const getTaskTemplatePublic = async (req: NextRequest, { params: { id } }: IdParams) => {
