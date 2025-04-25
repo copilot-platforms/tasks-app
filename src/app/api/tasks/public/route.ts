ca429e2fa356d87fd2c1bff6ecdd4120744479eb
Arpan Dhakal <arpandhakal20@gmail.com>
2025-04-25 15:23:39 +0545

    feat(OUT-1670) : implemented create task api for public
    - created deserialize method for converting public dto to create task request.
    - applied relevant validations.
    - deserialization for due date remanining.

--- src/app/api/tasks/public/route.ts
+++ src/app/api/tasks/public/route.ts
@@ -1,4 +1,5 @@
 import { withErrorHandler } from '@api/core/utils/withErrorHandler'
-import { getAllTasksPublic } from '@api/tasks/public/public.controller'
+import { createTaskPublic, getAllTasksPublic } from '@api/tasks/public/public.controller'
 
 export const GET = withErrorHandler(getAllTasksPublic)
+export const POST = withErrorHandler(createTaskPublic)
