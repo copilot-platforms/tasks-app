cd7c1986cb340bcb7978a503e4e73c2ec9a943d6
Rojan Rajbhandari <rrojan20@gmail.com>
2025-04-28 15:19:09 +0545

    feat(OUT-1681): add source web for load testing seeds

--- src/cmd/load-testing/load-testing.service.ts
+++ src/cmd/load-testing/load-testing.service.ts
@@ -11,7 +11,7 @@ import {
 import { CopilotAPI } from '@/utils/CopilotAPI'
 import { getRandomBool, getRandomFutureDate, getRandomFutureDateTime } from '@/utils/random'
 import { faker } from '@faker-js/faker'
-import { AssigneeType, PrismaClient, Task } from '@prisma/client'
+import { AssigneeType, PrismaClient, Source, Task } from '@prisma/client'
 import Bottleneck from 'bottleneck'
 import { z } from 'zod'
 
@@ -125,6 +125,7 @@ class LoadTester {
           assignedAt: getRandomBool() ? getRandomFutureDate() : null,
           createdAt: new Date(),
           dueDate: getRandomBool() ? getRandomFutureDateTime() : null,
+          source: Source.web,
         })
       }
       seedPromises.push(this.bottlenecks.db.schedule(() => this.db.task.createMany({ data })))
