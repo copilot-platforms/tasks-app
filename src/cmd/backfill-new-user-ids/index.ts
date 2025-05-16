129c749650b6f4de6c662295336ab1314f27836f
Rojan Rajbhandari <rrojan20@gmail.com>
2025-05-16 18:09:25 +0545

    feat(OUT-1742): add backfill script for new user ids

---
+++ src/cmd/backfill-new-user-ids/index.ts
@@ -0,0 +1,89 @@
+import { API_DOMAIN } from '@/constants/domains'
+import DBClient from '@/lib/db'
+import { AssigneeType } from '@prisma/client'
+import { z } from 'zod'
+
+interface Clientable {
+  id: string
+  companyId: string
+}
+
+const copilotAPIKey = process.env.COPILOT_API_KEY // Make sure to source the var beforehand
+const COPILOT_CLIENTS_ENDPOINT = `${API_DOMAIN}/v1/clients?limit=10000`
+
+const getCompanyMap = async (uniqueWorkspaceIds: Array<string>) => {
+  const workspaceClientCompanyIdMap: Record<string, Record<string, string>> = {}
+  const failedWorkspaces: Array<string> = []
+
+  // Fetch and set workspaceId + client-company id mapping
+  for (let workspaceId of uniqueWorkspaceIds) {
+    console.log(`backfill-company-id#run | Running backfill for tasks under workspace id ${workspaceId}`)
+    const resp = await fetch(COPILOT_CLIENTS_ENDPOINT, {
+      headers: {
+        'Content-Type': 'application/json',
+        'X-API-KEY': `${workspaceId}/${copilotAPIKey}`,
+      },
+    })
+    const clients: Clientable[] = (await resp.json())?.data
+    if (!clients) {
+      console.error(`backfill-company-id#run | Failed to fetch client data for ${workspaceId}`)
+      failedWorkspaces.push(workspaceId)
+      continue
+    }
+    workspaceClientCompanyIdMap[workspaceId] = clients.reduce(
+      (acc, client) => {
+        if (client.id && client.companyId) {
+          acc[client.id] = client.companyId
+        }
+        return acc
+      },
+      {} as Record<string, string>,
+    )
+  }
+  return { workspaceClientCompanyIdMap, failedWorkspaces }
+}
+
+const run = async () => {
+  console.log(`backfill-company-id#run | Using clients endpoint:`, COPILOT_CLIENTS_ENDPOINT)
+
+  const db = DBClient.getInstance()
+  const tasks = await db.task.findMany({
+    where: { assigneeType: { not: null } },
+  })
+
+  const uniqueWorkspaceIds = [...new Set(tasks.map((t) => t.workspaceId))]
+  console.log(`backfill-company-id#run | All workspace ids (${uniqueWorkspaceIds.length})`, uniqueWorkspaceIds)
+  // Map with workspaceId -> clientId -> companyId
+  const { workspaceClientCompanyIdMap, failedWorkspaces } = await getCompanyMap(uniqueWorkspaceIds)
+
+  const failedTasks: Array<string> = []
+  for (let task of tasks) {
+    if (failedWorkspaces.includes(task.workspaceId)) {
+      failedTasks.push(task.id)
+      continue
+    }
+
+    if (task.assigneeType === AssigneeType.internalUser) {
+      await db.task.update({
+        where: { id: task.id },
+        data: { internalUserId: task.assigneeId },
+      })
+    } else if (task.assigneeType === AssigneeType.client) {
+      const companyId = workspaceClientCompanyIdMap[task.workspaceId][z.string().uuid().parse(task.assigneeId)]
+      await db.task.update({
+        where: { id: task.id },
+        data: { clientId: task.assigneeId, companyId },
+      })
+    } else if (task.assigneeType === AssigneeType.company) {
+      await db.task.update({
+        where: { id: task.id },
+        data: { companyId: task.assigneeId },
+      })
+    }
+  }
+
+  console.log(`Failed workspaces (${failedWorkspaces.length})`, failedWorkspaces)
+  console.log(`Failed tasks (${failedTasks.length})`, failedTasks)
+}
+
+run()
