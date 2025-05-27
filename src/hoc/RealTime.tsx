23c14b96dd26895831c0338df7e83300a86f52a9
Rojan Rajbhandari <rrojan20@gmail.com>
2025-05-27 13:10:18 +0545

    feat(OUT-1785): implement refactored realtime task inserts

--- src/hoc/RealTime.tsx
+++ src/hoc/RealTime.tsx
@@ -71,61 +71,14 @@ export const RealTime = ({
     // Handle realtime subtasks in a modular way
     // TODO: Handle rest of the realtime operations in the same way in a TDB milestone
     const realtimeHandler = new RealtimeHandler(payload, user, userRole, redirectToBoard)
-    if (Object.keys(payload.new).includes('parentId') && (payload.new as RealTimeTaskResponse).parentId !== null) {
+    const isSubtask =
+      Object.keys(payload.new).includes('parentId') && (payload.new as RealTimeTaskResponse).parentId !== null
+
+    if (isSubtask) {
       return realtimeHandler.handleRealtimeSubtasks()
-    }
-
-    // TODO: Refactor all of this
-    if (payload.eventType === 'INSERT') {
-      // For both user types, filter out just tasks belonging to workspace.
-      // NOTE: This is not needed any more since we run `eq.${tokenPayload?.workspaceId}` in POSTGRES_CHANGES filter
-      let canUserAccessTask = payload.new.workspaceId === tokenPayload?.workspaceId
-
-      // If user is an internal user with client access limitations, they can only access tasks assigned to clients or company they have access to
-      if (user && userRole === AssigneeType.internalUser && InternalUsersSchema.parse(user).isClientAccessLimited) {
-        const assigneeSet = new Set(assignee.map((a) => a.id))
-        canUserAccessTask = canUserAccessTask && (payload.new.assigneeId ? assigneeSet.has(payload.new.assigneeId) : false) //filtering out unassigned tasks with a fallback false value.
-        // NOTE: isSubtask will never be true since `handleRealtimeSubtasks` takes care of it elsewhere
-        const isSubtask = payload.new.parentId && accesibleTaskIds.find((id) => id === payload.new.parentId) // dont append task into task list if its a subtask
-        if (canUserAccessTask) {
-          // @deprecated - use of accessibleTasks is preferred
-          store.dispatch(setAccesibleTaskIds([...accesibleTaskIds, payload.new.id]))
-          store.dispatch(setAccessibleTasks([...accessibleTasks, payload.new]))
-        }
-        canUserAccessTask = canUserAccessTask && !isSubtask
-      }
-      if (user && userRole === AssigneeType.internalUser && !InternalUsersSchema.parse(user).isClientAccessLimited) {
-        const isSubtask = !!payload.new.parentId
-        canUserAccessTask = canUserAccessTask && !isSubtask
-      }
-      // Additionally, if user is a client, it can only access tasks assigned to that client or the client's company
-      if (userRole === AssigneeType.client) {
-        canUserAccessTask = canUserAccessTask && [userId, tokenPayload?.companyId].includes(payload.new.assigneeId)
-        const isSubtask = payload.new.parentId && accesibleTaskIds.find((id) => id === payload.new.parentId)
-        if (canUserAccessTask) {
-          // @deprecated - use of accessibleTasks is preferred
-          store.dispatch(setAccesibleTaskIds([...accesibleTaskIds, payload.new.id]))
-          store.dispatch(setAccessibleTasks([...accessibleTasks, payload.new]))
-        }
-
-        canUserAccessTask = canUserAccessTask && !isSubtask
-      }
-
-      //check if the new task in this event belongs to the same workspaceId
-      if (canUserAccessTask && showUnarchived) {
-        store.dispatch(
-          setTasks([
-            // Remove any previously disjointed tasks from the board
-            ...tasks.filter((task) => task.parentId !== payload.new.id),
-            { ...payload.new, createdAt: new Date(payload.new.createdAt + 'Z') },
-          ]),
-        )
-        // NOTE: we append a Z here to make JS understand this raw timestamp (in format YYYY-MM-DD:HH:MM:SS.MS) is in UTC timezone
-        // New payloads listened on the 'INSERT' action in realtime doesn't contain this tz info so the order can mess up
-      }
-    }
-
-    if (payload.eventType === 'UPDATE') {
+    } else if (payload.eventType === 'INSERT') {
+      return realtimeHandler.handleRealtimeTaskInsert()
+    } else if (payload.eventType === 'UPDATE') {
       const updatedTask = payload.new
       // if (updatedTask.parentId && tasks.find((task) => task.id === updatedTask.parentId)) {
       //   return //short circuit if the updated task is a subtask and its parent id is in the tasks array
@@ -254,6 +207,8 @@ export const RealTime = ({
           }
         }
       }
+    } else {
+      console.error('Unknown event type for realtime handler')
     }
   }
 
