e40693c9419b364764aedaaaf7ce6c756b35e047
arpandhakal <arpandhakal20@gmail.com>
2025-05-30 12:32:03 +0545

    fix(OUT-1767) : applied requested changes on certain code quality changes

--- src/components/inputs/CopilotSelector.tsx
+++ src/components/inputs/CopilotSelector.tsx
@@ -3,13 +3,13 @@ import { selectTaskBoard } from '@/redux/features/taskBoardSlice'
 import 'copilot-design-system/dist/styles/main.css'
 import { UserCompanySelector, InputValue } from 'copilot-design-system'
 
-export const CopilotSelector = ({ onChange }: { onChange: (inputValue: InputValue[]) => void }) => {
+export const CopilotSelector = ({ onChange, name }: { onChange: (inputValue: InputValue[]) => void; name: string }) => {
   const { selectorAssignee } = useSelector(selectTaskBoard)
   return (
     <>
       <UserCompanySelector
         clientUsers={selectorAssignee.clients}
-        name="Assignee"
+        name={name}
         internalUsers={selectorAssignee.internalUsers}
         companies={selectorAssignee.companies}
         onChange={onChange}
