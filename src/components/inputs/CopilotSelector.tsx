81c97d79ff9c872b98bf99b48dd1f922eef4163d
arpandhakal <arpandhakal20@gmail.com>
2025-05-30 16:32:53 +0545

    feat(OUT-1781) : Integrated new selector component from copilot design system in task update on task card list.
    - created a new component - CopilotPopSelector, which allows custom React node + popper + copilotselector integration.
    - applied CopilotPopSelector in task card lists (list view cards and sub tasks list cards) with relevant data changes.
    - removed old selector and its dependency utilities form the TaskCardList component.

--- src/components/inputs/CopilotSelector.tsx
+++ src/components/inputs/CopilotSelector.tsx
@@ -1,7 +1,9 @@
-import { useSelector } from 'react-redux'
 import { selectTaskBoard } from '@/redux/features/taskBoardSlice'
+import { Box, Popper, Stack } from '@mui/material'
+import { InputValue, UserCompanySelector } from 'copilot-design-system'
 import 'copilot-design-system/dist/styles/main.css'
-import { UserCompanySelector, InputValue } from 'copilot-design-system'
+import { ReactNode, useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
 
 export const CopilotSelector = ({ onChange, name }: { onChange: (inputValue: InputValue[]) => void; name: string }) => {
   const { selectorAssignee } = useSelector(selectTaskBoard)
@@ -16,5 +18,69 @@ export const CopilotSelector = ({ onChange, name }: { onChange: (inputValue: Inp
         grouped={true}
       />
     </>
+  )
+}
+
+interface CopilotPopSelectorProps {
+  buttonContent?: ReactNode
+  disabled?: boolean
+  onClick?: () => void
+  name: string
+  onChange: (inputValue: InputValue[]) => void
+}
+export const CopilotPopSelector = ({ buttonContent, disabled = false, name, onChange }: CopilotPopSelectorProps) => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (!disabled) {
+      setAnchorEl(anchorEl ? null : event.currentTarget)
+    }
+  }
+  const open = Boolean(anchorEl)
+  const id = open ? 'selector-popper' : ''
+
+  useEffect(() => {
+    function closePopper(e: KeyboardEvent) {
+      if (e.key === 'Escape') {
+        setAnchorEl(null)
+      }
+    }
+
+    window.addEventListener('keydown', closePopper)
+
+    return () => {
+      window.removeEventListener('keydown', closePopper)
+    }
+  }, [])
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    if (event.key === 'Escape') {
+      setAnchorEl(null)
+    }
+  }
+  return (
+    <Stack direction="column">
+      <Box onMouseDown={handleClick}>
+        <> {buttonContent}</>
+      </Box>
+      <Popper
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        sx={{
+          width: 'fit-content',
+        }}
+        placement="bottom-start"
+      >
+        <Box onKeyDown={handleKeyDown}>
+          <CopilotSelector
+            name={name}
+            onChange={(inputValue) => {
+              onChange(inputValue)
+              setAnchorEl(null)
+            }}
+          />
+        </Box>
+      </Popper>
+    </Stack>
   )
 }
