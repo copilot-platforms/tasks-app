e75edb6ae84d6f925f8b95d784fcc888e73cc995
arpandhakal <arpandhakal20@gmail.com>
2025-06-05 15:47:39 +0545

    feat(OUT-1828) : implement poppper with button on places with assignee selector
    - implemented copilotPopSelector component instead of copilotSelector, raw UserCompanySelector component.
    - Added assigneeValue state and applied logic on chaning the assignee value on NewTaskForm, NewTaskCard and Sidebar assignee selection.
    - seggregated the selectorButton component from selector to make it usable for CopilotPopSelector.
    - Integrated new design with new assignee placeholder svgs on multiple places.
    - Some minor changes on types.

---
+++ src/components/buttons/SelectorButton.tsx
@@ -0,0 +1,73 @@
+import { Button } from '@mui/material'
+import { Property } from 'csstype'
+import { ReactNode } from 'react'
+
+export const SelectorButton = ({
+  buttonContent,
+  startIcon,
+  handleClick,
+  enableBackground,
+  outlined,
+  padding,
+  disabled,
+  height,
+  error,
+  endIcon,
+  cursor,
+}: {
+  startIcon?: ReactNode
+  buttonContent: ReactNode
+  handleClick?: () => void
+  enableBackground?: boolean
+  outlined?: boolean
+  padding?: string
+  disabled?: boolean
+  height?: string
+  error?: boolean
+  endIcon?: ReactNode
+  cursor?: Property.Cursor
+}) => {
+  return (
+    <Button
+      variant="outlined"
+      startIcon={startIcon ? startIcon : null}
+      endIcon={endIcon ? endIcon : null}
+      sx={(theme) => ({
+        textTransform: 'none',
+        border:
+          enableBackground || outlined
+            ? 'none'
+            : error
+              ? `1px solid ${theme.color.muiError}`
+              : `1px solid ${theme.color.borders.border}`,
+        bgcolor: enableBackground ? theme.color.gray[150] : '',
+        '&:hover': {
+          bgcolor: disabled ? 'white' : theme.color.gray[100],
+          border:
+            enableBackground || outlined
+              ? 'none'
+              : error
+                ? `1px solid ${theme.color.muiError}`
+                : `1px solid ${theme.color.borders.border}`,
+        },
+        '.MuiTouchRipple-child': {
+          bgcolor: theme.color.borders.border,
+        },
+        padding: padding ? padding : { xs: '2px 9px', md: '4px 16px' },
+        cursor: disabled ? 'auto' : (cursor ?? 'pointer'),
+        '& .MuiButton-startIcon': {
+          '& .MuiAvatar-root': {
+            fontSize: '14px',
+            fontWeight: '400',
+          },
+        },
+        height: height ?? '32px',
+      })}
+      onClick={handleClick}
+      disableRipple
+      disableTouchRipple
+    >
+      {buttonContent}
+    </Button>
+  )
+}
