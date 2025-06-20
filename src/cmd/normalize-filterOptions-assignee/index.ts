b5e42297e6f2598da48cae8802292e830b9f1378
arpandhakal <arpandhakal20@gmail.com>
2025-06-20 14:23:46 +0545

    fix(OUT-1909) : added bottleneck for backfilling filterOptions.assignee in viewsettings

--- src/cmd/normalize-filterOptions-assignee/index.ts
+++ src/cmd/normalize-filterOptions-assignee/index.ts
@@ -1,8 +1,11 @@
 import DBClient from '@/lib/db'
 import { emptyAssignee } from '@/utils/assignee'
+import Bottleneck from 'bottleneck'
 
 export const normalizeStringAssigneeToObject = async () => {
   const db = DBClient.getInstance()
+  const dbBottleneck = new Bottleneck({ minTime: 100, maxConcurrent: 5 })
+
   const viewSettingsList = await db.viewSetting.findMany()
 
   if (!viewSettingsList.length) {
@@ -13,7 +16,6 @@ export const normalizeStringAssigneeToObject = async () => {
     return (
       viewSetting?.filterOptions &&
       typeof viewSetting.filterOptions === 'object' &&
-      !Array.isArray(viewSetting.filterOptions) &&
       'assignee' in viewSetting.filterOptions &&
       typeof viewSetting.filterOptions.assignee === 'string'
     )
@@ -24,21 +26,27 @@ export const normalizeStringAssigneeToObject = async () => {
     console.info(`No changes required 🔥`)
     return
   }
+  const updatePromises = []
   for (const [index, viewSetting] of ViewSettingsWhereFilterByAssigneeIsString.entries()) {
     if (viewSetting) {
-      console.info(`updating ${index + 1} / ${ViewSettingsWhereFilterByAssigneeIsString.length} entries ⚒️`)
-      await db.viewSetting.update({
-        where: { id: viewSetting.id },
-        data: {
-          ...viewSetting,
-          filterOptions: {
-            ...(typeof viewSetting.filterOptions === 'object' ? viewSetting.filterOptions : {}),
-            assignee: emptyAssignee,
+      console.info(`queuing update ${index + 1} / ${ViewSettingsWhereFilterByAssigneeIsString.length} ⚒️`)
+      const updatePromise = dbBottleneck.schedule(() =>
+        db.viewSetting.update({
+          where: { id: viewSetting.id },
+          data: {
+            ...viewSetting,
+            filterOptions: {
+              ...(typeof viewSetting.filterOptions === 'object' ? viewSetting.filterOptions : {}),
+              assignee: emptyAssignee,
+            },
           },
-        },
-      })
+        }),
+      )
+      updatePromises.push(updatePromise)
     }
   }
+
+  await Promise.all(updatePromises)
 }
 
 normalizeStringAssigneeToObject()
