cd4dc9730f5ec4668d69f3e03b3f671575d972f0
Rojan Rajbhandari <rrojan20@gmail.com>
2025-06-26 16:06:41 +0545

    docs(OUT-1955): remove confusing comment

--- src/app/_cache/AssigneeCacheGetter.tsx
+++ src/app/_cache/AssigneeCacheGetter.tsx
@@ -10,7 +10,6 @@ interface ClientAssigneeCacheGetterProps {
 
 export const AssigneeCacheGetter = ({ lookupKey }: ClientAssigneeCacheGetterProps) => {
   useEffect(() => {
-    // The chance for a race condition here is very very low
     const assignee = localStorage.getItem(`assignees.${lookupKey}`)
     store.dispatch(setAssigneeList(assignee ? JSON.parse(assignee) : []))
   }, [lookupKey])
