5a55bba479107dfdc5407f71b236a8aa7636444b
arpandhakal <arpandhakal20@gmail.com>
2025-07-09 15:20:53 +0545

    fix(OUT-1996-re) : removed console log

--- src/lib/swr-config.ts
+++ src/lib/swr-config.ts
@@ -4,7 +4,6 @@ import { SWRConfiguration } from 'swr'
 
 export const swrConfig: SWRConfiguration = {
   onErrorRetry: (error, key, config, revalidate, { retryCount }) => {
-    console.log('retry')
     if (error.status === 404 || retryCount >= 3) return
     setTimeout(() => revalidate({ retryCount }), 500)
   },
