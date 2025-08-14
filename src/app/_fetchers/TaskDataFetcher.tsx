d2c814703a8051fe5c11a8c78cbd0fc4db98b676
Rojan Rajbhandari <rrojan20@gmail.com>
2025-08-14 16:46:10 +0545

    chore(OUT-2166): do not send showSubtasks to tasks API since implementation is in UI

--- src/app/_fetchers/TaskDataFetcher.tsx
+++ src/app/_fetchers/TaskDataFetcher.tsx
@@ -1,6 +1,6 @@
 import { selectTaskBoard, setIsTasksLoading, setTasks } from '@/redux/features/taskBoardSlice'
 import store from '@/redux/store'
-import { ArchivedOptionsType } from '@/types/dto/viewSettings.dto'
+import { DisplayOptions } from '@/types/dto/viewSettings.dto'
 import { PropsWithToken } from '@/types/interfaces'
 import { fetcher } from '@/utils/fetcher'
 import { useEffect } from 'react'
@@ -8,20 +8,29 @@ import { useSelector } from 'react-redux'
 import useSWR from 'swr'
 
 export const TaskDataFetcher = ({ token }: PropsWithToken) => {
-  const { showArchived, showUnarchived, tasks } = useSelector(selectTaskBoard)
+  const { showArchived, showUnarchived, showSubtasks, tasks } = useSelector(selectTaskBoard)
 
-  const buildQueryString = (token: string, archivedOptions?: ArchivedOptionsType) => {
+  const buildQueryString = (token: string, displayOptions?: DisplayOptions) => {
     const queryParams = new URLSearchParams({ token })
-    if (archivedOptions?.showArchived !== undefined) {
-      queryParams.append('showArchived', archivedOptions.showArchived.toString())
+    if (displayOptions?.showArchived !== undefined) {
+      queryParams.append('showArchived', displayOptions.showArchived.toString())
     }
-    if (archivedOptions?.showUnarchived !== undefined) {
-      queryParams.append('showUnarchived', archivedOptions.showUnarchived.toString())
+    if (displayOptions?.showUnarchived !== undefined) {
+      queryParams.append('showUnarchived', displayOptions.showUnarchived.toString())
     }
+
+    // NOTE: We don't need to send showSubtasks as a param to `getTasks` since we
+    // are currently implementing showSubtasks in UI only. Uncomment and proceed with handling
+    // showSubtasks in GET /api/tasks if we handle from the backend
+
+    // if (displayOptions?.showSubtasks !== undefined) {
+    //   queryParams.append('showSubtasks', displayOptions.showSubtasks.toString())
+    // }
+
     return queryParams.toString()
   }
 
-  const queryString = token ? buildQueryString(token, { showArchived, showUnarchived }) : null
+  const queryString = token ? buildQueryString(token, { showArchived, showUnarchived, showSubtasks }) : null
 
   const { data, isLoading } = useSWR(queryString ? `/api/tasks/?${queryString}` : null, fetcher, {
     fallbackData: { tasks },
