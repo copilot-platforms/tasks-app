9b31fdf76b8ed855c6066f2d2f39bbb1316a0d5f
arpandhakal-lgtm <arpandhakal20@gmail.com>
2025-08-20 16:37:32 +0545

    fix(OUT-2229): build error fixed

--- src/utils/getWorkspaceLabels.ts
+++ src/utils/getWorkspaceLabels.ts
@@ -4,18 +4,22 @@ export const getWorkspaceLabels = (workspace: WorkspaceResponse, shouldCapitaliz
   const capitalize = (str?: string) => (str ? str.charAt(0).toUpperCase() + str.slice(1) : str)
   const labels = shouldCapitalize
     ? {
-        individualTerm: workspace?.label.individualTerm ? capitalize(workspace?.label.individualTerm) : 'Client',
-        individualTermPlural: workspace?.label.individualTermPlural
-          ? capitalize(workspace?.label.individualTermPlural)
-          : 'Clients',
-        groupTerm: workspace?.label.groupTerm ? capitalize(workspace?.label.groupTerm) : 'Company',
-        groupTermPlural: workspace?.label.groupTermPlural ? capitalize(workspace?.label.groupTermPlural) : 'Companies',
+        individualTerm:
+          workspace.label && workspace.label.individualTerm ? capitalize(workspace.label.individualTerm) : 'Client',
+        individualTermPlural:
+          workspace.label && workspace.label.individualTermPlural
+            ? capitalize(workspace.label.individualTermPlural)
+            : 'Clients',
+        groupTerm: workspace.label && workspace.label.groupTerm ? capitalize(workspace.label.groupTerm) : 'Company',
+        groupTermPlural:
+          workspace.label && workspace.label.groupTermPlural ? capitalize(workspace.label.groupTermPlural) : 'Companies',
       }
     : {
-        individualTerm: workspace?.label.individualTerm ? workspace?.label.individualTerm : 'client',
-        individualTermPlural: workspace?.label.individualTermPlural ? workspace?.label.individualTermPlural : 'clients',
-        groupTerm: workspace?.label.groupTerm ? workspace?.label.groupTerm : 'company',
-        groupTermPlural: workspace?.label.groupTermPlural ? workspace?.label.groupTermPlural : 'companies',
+        individualTerm: workspace.label && workspace.label.individualTerm ? workspace.label.individualTerm : 'client',
+        individualTermPlural:
+          workspace.label && workspace.label.individualTermPlural ? workspace.label.individualTermPlural : 'clients',
+        groupTerm: workspace.label && workspace.label.groupTerm ? workspace.label.groupTerm : 'company',
+        groupTermPlural: workspace.label && workspace.label.groupTermPlural ? workspace.label.groupTermPlural : 'companies',
       }
 
   return labels
