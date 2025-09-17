99dfc4f183d389423afe7f6f3aa1e838fb0e2831
SandipBajracharya <sndp106.sb@gmail.com>
2025-09-17 16:34:06 +0545

    refactor(OUT-2358): removed tenary operator and direct return the bool value

--- src/utils/taskViewer.ts
+++ src/utils/taskViewer.ts
@@ -2,12 +2,12 @@ import { Token } from '@/types/common'
 import { Viewers } from '@/types/dto/tasks.dto'
 import { getPreviewMode } from './previewMode'
 
-export const checkIfTaskViewer = (viewers: Viewers, tokenPayload: Token | undefined) => {
-  return Array.isArray(viewers) &&
+export const checkIfTaskViewer = (viewers: Viewers, tokenPayload: Token | undefined): boolean => {
+  return (
+    Array.isArray(viewers) &&
     viewers.length > 0 &&
     (!viewers[0].clientId || viewers[0].clientId === tokenPayload?.clientId) &&
     viewers[0].companyId === tokenPayload?.companyId &&
     !getPreviewMode(tokenPayload)
-    ? true
-    : false
+  )
 }
