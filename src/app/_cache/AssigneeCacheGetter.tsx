bc45a89fb0a45e18f234f59933eb239a6865829c
arpandhakal-lgtm <arpandhakal20@gmail.com>
2025-09-17 14:05:00 +0545

    fix(OUT-2348): replaced localStorage for assignee caching with localforage. Also added a util to migrate existing data from localstorage to localforages indexedDb

--- src/app/_cache/AssigneeCacheGetter.tsx
+++ src/app/_cache/AssigneeCacheGetter.tsx
@@ -3,6 +3,7 @@
 import { setAssigneeList } from '@/redux/features/taskBoardSlice'
 import store from '@/redux/store'
 import { useEffect } from 'react'
+import { getAssignees, migrateAssignees } from '@/app/_cache/forageStorage'
 
 interface ClientAssigneeCacheGetterProps {
   lookupKey: string
@@ -10,8 +11,12 @@ interface ClientAssigneeCacheGetterProps {
 
 export const AssigneeCacheGetter = ({ lookupKey }: ClientAssigneeCacheGetterProps) => {
   useEffect(() => {
-    const assignee = localStorage.getItem(`assignees.${lookupKey}`)
-    store.dispatch(setAssigneeList(assignee ? JSON.parse(assignee) : []))
+    const run = async () => {
+      await migrateAssignees(lookupKey) //migrate from localStorage to localForage if required. Remember to remove this after a while.
+      const assignee = await getAssignees(lookupKey)
+      store.dispatch(setAssigneeList(assignee))
+    }
+    run()
   }, [lookupKey])
 
   return <></>
