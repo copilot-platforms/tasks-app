44b78e07cf3c492c3a2ce344135b481b75452462
arpandhakal-lgtm <arpandhakal20@gmail.com>
2025-09-22 09:15:03 +0545

    fix(OUT-2319): mark as read notification for viewers notification when task is completed

--- src/app/api/tasks/task-notifications.service.ts
+++ src/app/api/tasks/task-notifications.service.ts
@@ -259,6 +259,19 @@ export class TaskNotificationsService extends BaseService {
     if (updatedTask.assigneeType === AssigneeType.internalUser) {
       shouldCreateNotification &&
         (await this.notificationService.create(NotificationTaskActions.CompletedByIU, updatedTask, { disableEmail: true }))
+
+      const viewer = getTaskViewers(updatedTask)
+      if (!viewer) return
+      if (viewer?.clientId) {
+        try {
+          await this.notificationService.markClientNotificationAsRead(updatedTask)
+          return
+        } catch (e: unknown) {
+          console.error(`Failed to find ClientNotification for task ${updatedTask.id}`, e)
+        }
+      } else {
+        await this.notificationService.markAsReadForAllRecipients(updatedTask, NotificationTaskActions.SharedToCompany)
+      }
       // TODO: Clean code and handle notification center notification deletions here instead
     } else if (updatedTask.assigneeType === AssigneeType.company) {
       // Don't do this in parallel since this can cause rate-limits, each of them has their own bottlenecks for avoiding ratelimits
