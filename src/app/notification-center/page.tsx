f4c1e267ffd70fdd3aa0854a38febfb44f584abf
SandipBajracharya <sndp106.sb@gmail.com>
2025-10-09 10:30:37 +0545

    feat(OUT-2459): add main page for notification center

---
+++ src/app/notification-center/page.tsx
@@ -0,0 +1,32 @@
+import { SilentError } from '@/components/templates/SilentError'
+import { NotificationInProductCtaParamsSchema } from '@/types/common'
+import { UserType } from '@/types/interfaces'
+import { CopilotAPI } from '@/utils/CopilotAPI'
+import { redirectIfTaskCta } from '@/utils/redirect'
+import z from 'zod'
+
+async function getNotificationDetail(token: string) {
+  const copilot = new CopilotAPI(token)
+  const tokenPayload = await copilot.getTokenPayload()
+
+  if (!tokenPayload) throw new Error('Failed to get token payload')
+
+  return await copilot.getIUNotification(z.string().parse(tokenPayload.notificationId), tokenPayload.workspaceId) // notification "id" is expected in tokenPayload
+}
+
+export default async function NotificationCenter({ searchParams }: { searchParams: { token: string } }) {
+  const token = searchParams.token
+  if (!z.string().safeParse(token).success) {
+    return <SilentError message="Please provide a Valid Token" />
+  }
+
+  const notificationDetail = await getNotificationDetail(token)
+  if (!notificationDetail) return <SilentError message="Failed to get notification detail" />
+
+  const params = NotificationInProductCtaParamsSchema.parse(notificationDetail.deliveryTargets?.inProduct?.ctaParams)
+
+  redirectIfTaskCta(params, UserType.INTERNAL_USER, true)
+
+  // Silent Error is shown if redirect fails. Only possible reason for redirect to not work can be of the taskId not found
+  return <SilentError message="TaskId is not found" />
+}
