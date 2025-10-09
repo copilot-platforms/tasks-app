c003bfa0ce8dc7d5d1ba0bf813c3dc08312767df
arpandhakal-lgtm <arpandhakal20@gmail.com>
2025-10-09 17:11:08 +0545

    fix(OUT-2465): prevent description rerender on realtime changes for crm mode

--- src/app/_fetchers/OneTaskDataFetcher.tsx
+++ src/app/_fetchers/OneTaskDataFetcher.tsx
@@ -2,16 +2,19 @@
 
 import { setActiveTask } from '@/redux/features/taskBoardSlice'
 import store from '@/redux/store'
+import { TaskResponse } from '@/types/dto/tasks.dto'
 import { PropsWithToken } from '@/types/interfaces'
 import { fetcher } from '@/utils/fetcher'
+import { extractImgSrcs, replaceImgSrcs } from '@/utils/signedUrlReplacer'
 import { useEffect, useRef, useState } from 'react'
 import useSWR from 'swr'
 
 interface OneTaskDataFetcherProps extends PropsWithToken {
   task_id: string
+  initialTask: TaskResponse
 }
 
-export const OneTaskDataFetcher = ({ token, task_id }: OneTaskDataFetcherProps & PropsWithToken) => {
+export const OneTaskDataFetcher = ({ token, task_id, initialTask }: OneTaskDataFetcherProps & PropsWithToken) => {
   const hasDispatchedRef = useRef(false)
   const buildQueryString = (token: string) => {
     const queryParams = new URLSearchParams({ token })
@@ -29,7 +32,18 @@ export const OneTaskDataFetcher = ({ token, task_id }: OneTaskDataFetcherProps &
   useEffect(() => {
     if (!hasDispatchedRef.current && data?.task) {
       //only invalidate cache on mount.
-      store.dispatch(setActiveTask(data.task))
+      const newTask = structuredClone(data.task)
+      if (initialTask?.body && newTask.body === undefined) {
+        newTask.body = initialTask?.body
+      }
+      if (initialTask && initialTask.body && newTask.body) {
+        const oldImgSrcs = extractImgSrcs(initialTask.body)
+        const newImgSrcs = extractImgSrcs(newTask.body)
+        if (oldImgSrcs.length > 0 && newImgSrcs.length > 0) {
+          newTask.body = replaceImgSrcs(newTask.body, newImgSrcs, oldImgSrcs)
+        }
+      }
+      store.dispatch(setActiveTask(newTask))
       hasDispatchedRef.current = true
     }
   }, [data])
