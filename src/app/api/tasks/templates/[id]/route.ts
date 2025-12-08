bb7228b923bf299f5498cfe3c632a66d001414e4
arpandhakal-lgtm <arpandhakal20@gmail.com>
2025-12-08 17:18:26 +0545

    feat(OUT-2754): support subTask templates for templates
    - modified tasktemplates modal to support new fields subTaskTemplates, parentId with proper relations.
    - added a mechanism (API) to create subTaskTemplates for a template with proper validation.
    - Modified retrieve template and list templates request's response to include parentId and subTaskTemplates.
    - Also added support for getting subTaskTemplates in response for public facing APIs.

--- src/app/api/tasks/templates/[id]/route.ts
+++ src/app/api/tasks/templates/[id]/route.ts
@@ -1,5 +1,6 @@
 import { withErrorHandler } from '@api/core/utils/withErrorHandler'
-import { deleteTaskTemplate, updateTaskTemplate } from '@api/tasks/templates/templates.controller'
+import { createSubTaskTemplate, deleteTaskTemplate, updateTaskTemplate } from '@api/tasks/templates/templates.controller'
 
 export const PATCH = withErrorHandler(updateTaskTemplate)
 export const DELETE = withErrorHandler(deleteTaskTemplate)
+export const POST = withErrorHandler(createSubTaskTemplate)
