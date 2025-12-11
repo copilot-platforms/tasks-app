5ff0470c922d0c47595edda9ca6bc05de33e54da
arpandhakal-lgtm <arpandhakal20@gmail.com>
2025-12-11 15:40:26 +0545

    fix(OUT-2752): app bridge not loading functions from react client component

--- src/app/manage-templates/ui/ManageTemplatesDetailsAppBridge.tsx
+++ src/app/manage-templates/ui/ManageTemplatesDetailsAppBridge.tsx
@@ -8,6 +8,7 @@ import { setShowConfirmDeleteModal } from '@/redux/features/taskDetailsSlice'
 import { setCreateTemplateFields, setTargetTemplateId } from '@/redux/features/templateSlice'
 import store from '@/redux/store'
 import { ITemplate } from '@/types/interfaces'
+import { useCallback, useEffect, useState } from 'react'
 
 interface ManageTemplateDetailsAppBridgeProps {
   portalUrl?: string
@@ -23,11 +24,22 @@ export const ManageTemplateDetailsAppBridge = ({
   template,
   breadcrumbItems,
 }: ManageTemplateDetailsAppBridgeProps) => {
-  const handleDeleteTemplate = () => {
+  const [awake, setAwake] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAwake(true)
+    }, 0)
+  }, [])
+
+  const handleDeleteTemplate = useCallback(() => {
     store.dispatch(setShowConfirmDeleteModal())
     store.dispatch(setTargetTemplateId(template.id))
     store.dispatch(setCreateTemplateFields({ targetField: 'taskName', value: template.title }))
-  }
+    // "awaken" callback using one more render to avoid hydration issues
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [awake])
+
   const items: Clickable[] = [
     {
       label: 'Delete template',
