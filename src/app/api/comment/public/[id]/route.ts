c540cacc2f702ae9eb6a588dcb03f7fad76eb447
SandipBajracharya <sndp106.sb@gmail.com>
2026-01-15 12:21:36 +0545

    feat(OUT-2919): create public api to read single comment of a task
    - [x] accept comment id as path variable
    - [x] include attachments in response
    - [x] serialize response data

---
+++ src/app/api/comment/public/[id]/route.ts
@@ -0,0 +1,4 @@
+import { getOneCommentPublicForTask } from '@/app/api/comment/public/comment-public.controller'
+import { withErrorHandler } from '@/app/api/core/utils/withErrorHandler'
+
+export const GET = withErrorHandler(getOneCommentPublicForTask)
