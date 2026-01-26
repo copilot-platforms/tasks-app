e852ead7680d53c86c36d12629a5bde438623d95
SandipBajracharya <sndp106.sb@gmail.com>
2026-01-26 12:30:57 +0545

    refactor(OUT-2923): rename classes, remove functions with same functionality

--- src/app/api/tasks/public/[id]/comments/route.ts
+++ src/app/api/tasks/public/[id]/comments/route.ts
@@ -1,4 +1,4 @@
-import { getAllCommentsPublic } from '@/app/api/comment/public/comment-public.controller'
+import { getAllCommentsPublic } from '@/app/api/comment/public/public.controller'
 import { withErrorHandler } from '@/app/api/core/utils/withErrorHandler'
 
 export const GET = withErrorHandler(getAllCommentsPublic)
