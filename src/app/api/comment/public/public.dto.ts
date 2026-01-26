c48c715b664e26083d1ce61f011e7ad1067aa85f
SandipBajracharya <sndp106.sb@gmail.com>
2026-01-26 12:30:57 +0545

    refactor(OUT-2923): rename classes, remove functions with same functionality

--- src/app/api/comment/public/public.dto.ts
+++ src/app/api/comment/public/public.dto.ts
@@ -1,19 +1,7 @@
+import { PublicAttachmentDtoSchema } from '@/app/api/attachments/public/public.dto'
 import { RFC3339DateSchema } from '@/types/common'
 import { AssigneeType } from '@prisma/client'
 import z from 'zod'
-
-export const PublicAttachmentDtoSchema = z.object({
-  id: z.string().uuid(),
-  fileName: z.string(),
-  fileSize: z.number(),
-  mimeType: z.string(),
-  downloadUrl: z.string().url().nullable(),
-  uploadedBy: z.string().uuid(),
-  uploadedByUserType: z.nativeEnum(AssigneeType).nullable(),
-  uploadedDate: RFC3339DateSchema,
-  deletedDate: RFC3339DateSchema.nullable(),
-})
-export type PublicAttachmentDto = z.infer<typeof PublicAttachmentDtoSchema>
 
 export const PublicCommentDtoSchema = z.object({
   id: z.string().uuid(),
