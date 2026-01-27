52505308c35f111fd224651a2fffd7a06e443dbf
arpandhakal-lgtm <arpandhakal20@gmail.com>
2026-01-27 16:03:00 +0545

    fix(OUT-3009): applied requested changes, changed api route from api/comment to api/comments

--- src/app/api/comment/[id]/route.ts
+++
@@ -1,5 +0,0 @@
-import { withErrorHandler } from '@api/core/utils/withErrorHandler'
-import { deleteComment, updateComment } from '@api/comment/comment.controller'
-
-export const PATCH = withErrorHandler(updateComment)
-export const DELETE = withErrorHandler(deleteComment)
